f83b1c851c3e90a3521b9707a6af3bcc56769698
Hanul Kim <hanulkim2000@naver.com>
2023-09-11 16:32:07 +0900

    chore:react-router set up

--- coffeeing/src/App.tsx
+++ coffeeing/src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Routes,Route,useNavigate} from 'react-router-dom'
+import SignupPage from 'pages/SignupPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>커피잉</div>
+
+      {/* Routes */}
+      <Routes>
+        <Route path='/signup' element={<SignupPage/>}></Route>
+      </Routes>
     </div>
   );
 }
