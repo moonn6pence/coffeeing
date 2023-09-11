e884d03491f39769e6c0d0889cc4236fd2abfdab
taeyong <amico741@naver.com>
2023-09-11 16:00:25 +0900

    feat: 피드 엔티티 클래스 추가

---
+++ coffeeing/src/main/java/com/ssafy/coffeeing/modules/feed/domain/Feed.java
@@ -0,0 +1,33 @@
+package com.ssafy.coffeeing.modules.feed.domain;
+
+import com.ssafy.coffeeing.modules.member.domain.Member;
+import com.ssafy.coffeeing.modules.util.base.BaseEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+
+@Getter
+@SuperBuilder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AttributeOverride(name = "id", column = @Column(name = "feed_id"))
+@Entity
+public class Feed extends BaseEntity {
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @Column(name = "like_count", nullable = false)
+    private Long likeCount;
+
+    @Column(name = "image_url", nullable = false)
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+}
