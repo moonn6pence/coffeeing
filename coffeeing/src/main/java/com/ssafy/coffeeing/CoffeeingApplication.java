f5f7f2171fda37779defde2df90e01e9ee8284e2
신현철 <shin421179@gmail.com>
2023-09-11 14:58:33 +0900

    fix : 엔트리포인트 클래스명 수정

--- coffeeing/src/main/java/com/ssafy/coffeeing/CoffeeingApplication.java
+++ coffeeing/src/main/java/com/ssafy/coffeeing/CoffeeingApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-public class CoffeingApplication {
+public class CoffeeingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CoffeingApplication.class, args);
+		SpringApplication.run(CoffeeingApplication.class, args);
 	}
 
 }
