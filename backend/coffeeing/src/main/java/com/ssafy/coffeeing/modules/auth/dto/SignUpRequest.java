8a62a3d510a008d92a058417016f61e51665ad3f
khs <anjfkrhdkdle@naver.com>
2023-09-12 17:29:53 +0900

    fix: 회원가입 validation 추가

--- backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/auth/dto/SignUpRequest.java
+++ backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/auth/dto/SignUpRequest.java
@@ -1,7 +1,9 @@
 package com.ssafy.coffeeing.modules.auth.dto;
 
+import javax.validation.constraints.Pattern;
+
 public record SignUpRequest(
-	String email,
-	String password
+	@Pattern(regexp = "^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$") String email,
+	@Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$") String password
 ) {
 }
