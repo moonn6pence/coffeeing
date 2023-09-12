d8b38661b0374b4b1d728dc272d4fb15c7b45e41
khs <anjfkrhdkdle@naver.com>
2023-09-12 17:31:15 +0900

    feat: 닉네임 중복 조회 및 onboarding 추가정보 입력 api 추가

---
+++ backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/member/controller/MemberController.java
@@ -0,0 +1,42 @@
+package com.ssafy.coffeeing.modules.member.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.coffeeing.modules.member.dto.ExistNickNameResponse;
+import com.ssafy.coffeeing.modules.member.dto.OnboardRequest;
+import com.ssafy.coffeeing.modules.member.dto.OnboardResponse;
+import com.ssafy.coffeeing.modules.member.service.MemberService;
+import com.ssafy.coffeeing.modules.util.base.BaseResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/member")
+public class MemberController {
+
+	private final MemberService memberService;
+
+	@GetMapping("/unique-nickname")
+	public BaseResponse<ExistNickNameResponse> checkDuplicateNickname(@RequestParam String nickname) {
+		return BaseResponse.<ExistNickNameResponse>builder()
+			.data(memberService.checkDuplicateNickname(nickname))
+			.build();
+	}
+
+	@PostMapping("/onboard")
+	public BaseResponse<OnboardResponse> insertAdditionalMemberInfo(Authentication authentication, @Valid @RequestBody OnboardRequest onboardRequest) {
+
+		return BaseResponse.<OnboardResponse>builder()
+			.data(memberService.insertAdditionalMemberInfo(authentication.getName(), onboardRequest))
+			.build();
+	}
+}
---
+++ backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/member/service/MemberService.java
@@ -0,0 +1,35 @@
+package com.ssafy.coffeeing.modules.member.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ssafy.coffeeing.modules.global.exception.BusinessException;
+import com.ssafy.coffeeing.modules.global.exception.info.MemberErrorInfo;
+import com.ssafy.coffeeing.modules.member.domain.Member;
+import com.ssafy.coffeeing.modules.member.domain.MemberState;
+import com.ssafy.coffeeing.modules.member.dto.ExistNickNameResponse;
+import com.ssafy.coffeeing.modules.member.dto.OnboardRequest;
+import com.ssafy.coffeeing.modules.member.dto.OnboardResponse;
+import com.ssafy.coffeeing.modules.member.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+	private final MemberRepository memberRepository;
+
+	@Transactional(readOnly = true)
+	public ExistNickNameResponse checkDuplicateNickname(final String nickname) {
+		return new ExistNickNameResponse(memberRepository.existsByNickname(nickname));
+	}
+
+	@Transactional
+	public OnboardResponse insertAdditionalMemberInfo(final String email, final OnboardRequest onboardRequest) {
+		Member member = memberRepository.findByEmail(email).orElseThrow(()->new BusinessException(MemberErrorInfo.NOT_FOUND));
+		member.updateMemberState(MemberState.BEFORE_RESEARCH);
+		member.updateByOnboardResult(onboardRequest.nickname(), onboardRequest.ageIdx(), onboardRequest.genderIdx());
+		return new OnboardResponse(member.getId(),member.getNickname());
+	}
+}
