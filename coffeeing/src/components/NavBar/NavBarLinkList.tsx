ecbedb0eef1c70e3490b68fce1e29558894d7ef3
hyuna <kook980316@gmail.com>
2023-09-12 18:00:50 +0900

    feat: navbar 용 링크 리스트 추가

---
+++ coffeeing/src/components/NavBar/NavBarLinkList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const NavBarLinkList = () => {
+  const navigation = [
+    { name: '원두/캡슐', href: '#', current: true },
+    { name: '추천', href: '#', current: false },
+    { name: '피드', href: '#', current: false },
+    { name: '검색', href: '#', current: false },
+  ];
+
+  return <div></div>;
+};
