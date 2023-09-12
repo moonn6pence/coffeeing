179565de5405861a47e9d3788fed08170a469109
Hanul Kim <hanulkim2000@naver.com>
2023-09-12 13:02:22 +0900

    refactor: input type props 추가

--- coffeeing/src/components/InputField.tsx
+++ coffeeing/src/components/InputField.tsx
@@ -4,18 +4,19 @@ type InputFieldProps = {
   label:string;
   placeholder:string;
   value:string;
+  type:string;
   onChange:(e:ChangeEvent<HTMLInputElement>)=>void;
 }
 
-function InputField({ label, placeholder, value, onChange }:InputFieldProps) {
+function InputField({ label, placeholder, value, type, onChange }:InputFieldProps) {
   return (
     <div className="flex flex-col gap-1">
       <label className="text-base text-gray-800 font-medium">{label}</label>
       <input
         className="w-96 h-14 border border-gray-400 pl-4 rounded-xl"
-        type="text"
         placeholder={placeholder}
         value={value}
+        type={type}
         onChange={onChange}
       />
     </div>
