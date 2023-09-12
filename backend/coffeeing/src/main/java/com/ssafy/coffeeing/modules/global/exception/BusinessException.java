da7571683718f5d4f829b36cf2b65c0e6300210b
khs <anjfkrhdkdle@naver.com>
2023-09-12 14:57:53 +0900

    fix: Business Exception 생성자 추가

--- backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/global/exception/BusinessException.java
+++ backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/global/exception/BusinessException.java
@@ -1,9 +1,12 @@
 package com.ssafy.coffeeing.modules.global.exception;
 
 import com.ssafy.coffeeing.modules.global.exception.info.ErrorInfo;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class BusinessException extends RuntimeException{
 
     private ErrorInfo info;
