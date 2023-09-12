003a7e95ac861bcc9e77dc344a5defce75dad7d6
Hanul Kim <hanulkim2000@naver.com>
2023-09-12 13:02:38 +0900

    feat:login page 추가

---
+++ coffeeing/src/pages/LoginPage.tsx
@@ -0,0 +1,53 @@
+import React,{useState,MouseEvent} from "react";
+import {useNavigate} from 'react-router-dom'
+import InputField from "components/InputField";
+import Button from "components/Button";
+import GoogleLoginBtn from "components/GoogleLogin";
+
+function LoginPage() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [pw, setPw] = useState('');
+  // 로그인 처리
+  const handleSubmit = (e:MouseEvent)=>{
+    e.preventDefault();
+    console.log('로그인 처리')
+  }
+
+  return(
+    <div className="flex flex-col gap-6 items-center">
+      <div className="text-3xl font-bold">로그인</div>
+      <div className="flex flex-row gap-1">
+        <p>아직 회원이 아니신가요?</p>
+        <p className="hover:font-bold cursor-pointer" onClick={()=>{navigate('/signup')}}>회원가입</p>
+      </div>
+
+      <InputField
+        label="이메일"
+        placeholder="이메일을 입력해주세요."
+        value={email}
+        type='email'
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <InputField
+        label="비밀번호"
+        placeholder="비밀번호를 입력해주세요."
+        value={pw}
+        type='password'
+        onChange={(e) => setPw(e.target.value)}
+      />
+      <div className="flex flex-col items-center gap-2">
+        <Button placeholder="로그인" handleSubmit={handleSubmit}/>
+        <p className="text-sm flex flex-row gap-2">
+          <span>비밀번호를 잊어버리셨나요?</span>
+          <span className="font-semibold">비밀번호 찾기</span>
+        </p>
+      </div>
+      
+      <div className="text-xl">OR</div>
+      <GoogleLoginBtn/>
+    </div>
+  )
+}
+
+export default LoginPage;
