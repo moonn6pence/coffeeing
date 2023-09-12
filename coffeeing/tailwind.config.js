0245b4fc4d1e22bf44b02de497fc1f055dbb6f5f
hyuna <kook980316@gmail.com>
2023-09-12 16:09:41 +0900

    design: tailwind 사용자 서식 추가

--- coffeeing/tailwind.config.js
+++ coffeeing/tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        // capsule card 용 w, h
+        70.5: '17.625rem',
+        94: '23.5rem',
+      },
+      colors: {
+        'font-gray': '#4D4D4D',
+      },
+    },
   },
   plugins: [],
-}
-
+};
