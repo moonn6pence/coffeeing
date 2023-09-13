6686d99a9715126d4941249c397ffd6ff6a3e1de
taeyong <amico741@naver.com>
2023-09-13 11:16:11 +0900

    test: Feed 업로드 테스트 추가

---
+++ backend/coffeeing/src/test/java/com/ssafy/coffeeing/dummy/FeedTestDummy.java
@@ -0,0 +1,17 @@
+package com.ssafy.coffeeing.dummy;
+
+import com.ssafy.coffeeing.modules.feed.dto.ImageElement;
+import com.ssafy.coffeeing.modules.feed.dto.UploadFeedRequest;
+
+import java.util.List;
+
+public class FeedTestDummy {
+
+    public static UploadFeedRequest createUploadFeedRequest() {
+        return new UploadFeedRequest(createImageElement(), "첫 번째 피드1");
+    }
+
+    public static List<ImageElement> createImageElement() {
+        return List.of(new ImageElement("https://image1.png"), new ImageElement("https://image2.png"));
+    }
+}
---
+++ backend/coffeeing/src/test/java/com/ssafy/coffeeing/modules/feed/service/FeedServiceTest.java
@@ -0,0 +1,38 @@
+package com.ssafy.coffeeing.modules.feed.service;
+
+import com.ssafy.coffeeing.dummy.FeedTestDummy;
+import com.ssafy.coffeeing.dummy.MemberTestDummy;
+import com.ssafy.coffeeing.modules.feed.dto.UploadFeedRequest;
+import com.ssafy.coffeeing.modules.feed.dto.UploadFeedResponse;
+import com.ssafy.coffeeing.modules.member.repository.MemberRepository;
+import com.ssafy.coffeeing.modules.util.ServiceTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class FeedServiceTest extends ServiceTest {
+
+    @Autowired
+    private FeedService feedService;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @DisplayName("피드 업로드 요청 시, 업로드에 성공한다.")
+    @Test
+    void Given_UploadFeedRequest_When_SaveFeed_Then_Success() {
+        //given
+        memberRepository.save(MemberTestDummy.createGeneralMember());
+        UploadFeedRequest uploadFeedRequest = FeedTestDummy.createUploadFeedRequest();
+
+        //when
+        UploadFeedResponse uploadFeedResponse = feedService
+                .uploadFeedByMember(uploadFeedRequest.content(), uploadFeedRequest.images());
+
+        //then
+        assertThat(uploadFeedResponse.feedId()).isPositive();
+    }
+}
