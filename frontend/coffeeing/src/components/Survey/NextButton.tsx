f9acea41d0cb04033d1ce6ec98ac2d8605705e83
Hanul Kim <hanulkim2000@naver.com>
2023-09-13 17:45:12 +0900

    feat:survey 다음 버튼 추가

---
+++ frontend/coffeeing/src/components/Survey/NextButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const NextButton = ()=>{
+  return(
+    <div>
+      <button className='w-40 h-12 text-white bg-light-roasting text-xl'>다음</button>
+    </div>
+  )
+}
