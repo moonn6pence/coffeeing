e02f0d5498a8af788b8791c09246c60a180e7dce
김태용 <amico741@naver.com>
2023-09-14 01:57:40 +0900

    feat: 피드 삭제 및 업데이트 컨트롤러 로직 추가 및 validation추가

--- backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/feed/controller/FeedController.java
+++ backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/feed/controller/FeedController.java
@@ -1,14 +1,13 @@
 package com.ssafy.coffeeing.modules.feed.controller;
 
+import com.ssafy.coffeeing.modules.feed.dto.UpdateFeedRequest;
 import com.ssafy.coffeeing.modules.feed.dto.UploadFeedRequest;
 import com.ssafy.coffeeing.modules.feed.dto.UploadFeedResponse;
 import com.ssafy.coffeeing.modules.feed.service.FeedService;
 import com.ssafy.coffeeing.modules.util.base.BaseResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -23,6 +22,22 @@ public class FeedController {
     public BaseResponse<UploadFeedResponse> uploadFeedRequest(@Valid @RequestBody UploadFeedRequest uploadFeedRequest) {
         return BaseResponse.<UploadFeedResponse>builder()
                 .data(feedService.uploadFeedByMember(uploadFeedRequest))
+                .build();
+    }
+
+    @PatchMapping("/{feedId}")
+    public BaseResponse<Void> updateFeedContent(
+            @PathVariable @NumberFormat Long feedId,
+            @Valid @RequestBody UpdateFeedRequest updateFeedRequest) {
+        feedService.updateFeedContentById(feedId, updateFeedRequest);
+        return BaseResponse.<Void>builder()
+                .build();
+    }
+
+    @DeleteMapping("/{feedId}")
+    public BaseResponse<Void> deleteFeed(@PathVariable @NumberFormat Long feedId) {
+        feedService.deleteFeedById(feedId);
+        return BaseResponse.<Void>builder()
                 .build();
     }
 }
--- backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/feed/dto/UpdateFeedRequest.java
+++ backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/feed/dto/UpdateFeedRequest.java
@@ -1,6 +1,7 @@
 package com.ssafy.coffeeing.modules.feed.dto;
 
+import org.hibernate.validator.constraints.Length;
+
 public record UpdateFeedRequest(
-        String content
-) {
-}
+        @Length(max = 2200) String content
+) { }
