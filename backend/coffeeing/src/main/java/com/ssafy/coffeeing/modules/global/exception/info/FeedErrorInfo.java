9dd286afa13be0d94c29052c0e1095d5d9bb86cf
김태용 <amico741@naver.com>
2023-09-13 21:32:41 +0900

    fix: BusinessException 문구 수정

--- backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/global/exception/info/FeedErrorInfo.java
+++ backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/global/exception/info/FeedErrorInfo.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum FeedErrorInfo implements ErrorInfo {
-    AWS_S3_CLIENT_EXCEPTION("800", "서버에서 PRESIGNED_URL 생성하는데 문제가 발생했습니다."),
-    FEED_IMAGES_TO_JSON_STRING_ERROR("801", "서버에서 이미지 URL 처리 문제가 발생했습니다");
+    AWS_S3_CLIENT_EXCEPTION("800", "서버에서 예상치 못한 문제가 발생했습니다."),
+    FEED_IMAGES_TO_JSON_STRING_ERROR("801", "서버에서 예상치 못한 문제가 발생했습니다.");
 
     private final String code;
     private final String message;
