4e89a02c61ce6502db292da69211fcfafe29038f
hyuna <kook980316@gmail.com>
2023-09-13 10:46:14 +0900

    feat: navbar title 빼고 틀 추가

--- coffeeing/src/components/NavBar/NavBarBody.tsx
+++ coffeeing/src/components/NavBar/NavBarBody.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { NavBarButton } from './NavBarButton';
+import { NavBarLinkList } from './NavBarLinkList';
 
 export const NavBarBody = () => {
   return (
-    <div className="w-screen h-16 border-b border-light-roasting flex py-2 px-30">
-      <NavBarButton value="로그인" navLink="/login" />
-      <NavBarButton value="회원가입" navLink="/signup" />
+    <div className="w-screen h-16 border-b border-light-roasting flex justify-around py-2 px-30">
+      <NavBarLinkList />
+      <div className="flex space-x-3">
+        <NavBarButton value="로그인" navLink="/login" />
+        <NavBarButton value="회원가입" navLink="/signup" />
+      </div>
     </div>
   );
 };
