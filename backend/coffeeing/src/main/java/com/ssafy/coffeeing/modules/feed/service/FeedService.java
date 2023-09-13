19cf263876f4fbe7d0b5f3fde0d980ebd18be451
YongsHub <amico741@naver.com>
2023-09-13 16:26:23 +0900

    fix: Member 인스턴스 SecurityContextUtils 이용하도록 변경

--- backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/feed/service/FeedService.java
+++ backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/feed/service/FeedService.java
@@ -3,34 +3,29 @@ package com.ssafy.coffeeing.modules.feed.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.coffeeing.modules.feed.domain.Feed;
-import com.ssafy.coffeeing.modules.feed.dto.ImageElement;
 import com.ssafy.coffeeing.modules.feed.dto.UploadFeedRequest;
 import com.ssafy.coffeeing.modules.feed.dto.UploadFeedResponse;
 import com.ssafy.coffeeing.modules.feed.mapper.FeedMapper;
 import com.ssafy.coffeeing.modules.feed.repository.FeedRepository;
 import com.ssafy.coffeeing.modules.global.exception.BusinessException;
 import com.ssafy.coffeeing.modules.global.exception.info.FeedErrorInfo;
-import com.ssafy.coffeeing.modules.global.exception.info.MemberErrorInfo;
+import com.ssafy.coffeeing.modules.global.security.util.SecurityContextUtils;
 import com.ssafy.coffeeing.modules.member.domain.Member;
-import com.ssafy.coffeeing.modules.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class FeedService {
 
     private final FeedRepository feedRepository;
-    private final MemberRepository memberRepository;
+    private final SecurityContextUtils securityContextUtils;
 
     @Transactional
     public UploadFeedResponse uploadFeedByMember(UploadFeedRequest uploadFeedRequest) {
         ObjectMapper objectMapper = new ObjectMapper();
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new BusinessException(MemberErrorInfo.NOT_FOUND));
+        Member member = securityContextUtils.getCurrnetAuthenticatedMember();
 
         try{
             String imageUrl = objectMapper.writeValueAsString(uploadFeedRequest.images());
--- backend/coffeeing/src/test/java/com/ssafy/coffeeing/modules/feed/service/FeedServiceTest.java
+++ backend/coffeeing/src/test/java/com/ssafy/coffeeing/modules/feed/service/FeedServiceTest.java
@@ -4,18 +4,16 @@ import com.ssafy.coffeeing.dummy.FeedTestDummy;
 import com.ssafy.coffeeing.dummy.MemberTestDummy;
 import com.ssafy.coffeeing.modules.feed.dto.UploadFeedRequest;
 import com.ssafy.coffeeing.modules.feed.dto.UploadFeedResponse;
+import com.ssafy.coffeeing.modules.global.security.util.SecurityContextUtils;
 import com.ssafy.coffeeing.modules.member.domain.Member;
 import com.ssafy.coffeeing.modules.member.repository.MemberRepository;
 import com.ssafy.coffeeing.modules.util.ServiceTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.SpyBean;
-
-import java.util.Optional;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -26,14 +24,19 @@ class FeedServiceTest extends ServiceTest {
     @Autowired
     private FeedService feedService;
 
-    @SpyBean
+    @Autowired
     private MemberRepository memberRepository;
+
+    @MockBean
+    private SecurityContextUtils securityContextUtils;
 
     @DisplayName("피드 업로드 요청 시, 업로드에 성공한다.")
     @Test
     void Given_UploadFeedRequest_When_SaveFeed_Then_Success() {
         //given
-        given(memberRepository.findById(any())).willReturn(Optional.of(MemberTestDummy.createGeneralMember()));
+        Member member = memberRepository.save(MemberTestDummy.createGeneralMember());
+        given(securityContextUtils.getCurrnetAuthenticatedMember())
+                .willReturn(member);
         UploadFeedRequest uploadFeedRequest = FeedTestDummy.createUploadFeedRequest();
 
         //when
@@ -44,6 +47,6 @@ class FeedServiceTest extends ServiceTest {
         assertThat(uploadFeedResponse.feedId()).isPositive();
 
         //verify
-        verify(memberRepository, times(1)).findById(any());
+        verify(securityContextUtils, times(1)).getCurrnetAuthenticatedMember();
     }
 }
