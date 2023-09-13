dd427c0dafc7b1bd145e4a2605cebe63840b91ba
taeyong <amico741@naver.com>
2023-09-13 13:55:28 +0900

    feat: Feed 업로드 비즈니스 로직 추가

--- backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/feed/service/FeedService.java
+++ backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/feed/service/FeedService.java
@@ -1,11 +1,42 @@
 package com.ssafy.coffeeing.modules.feed.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssafy.coffeeing.modules.feed.domain.Feed;
+import com.ssafy.coffeeing.modules.feed.dto.ImageElement;
+import com.ssafy.coffeeing.modules.feed.dto.UploadFeedResponse;
+import com.ssafy.coffeeing.modules.feed.mapper.FeedMapper;
+import com.ssafy.coffeeing.modules.feed.repository.FeedRepository;
+import com.ssafy.coffeeing.modules.global.exception.BusinessException;
+import com.ssafy.coffeeing.modules.global.exception.info.FeedErrorInfo;
+import com.ssafy.coffeeing.modules.global.exception.info.MemberErrorInfo;
+import com.ssafy.coffeeing.modules.member.domain.Member;
+import com.ssafy.coffeeing.modules.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class FeedService {
 
+    private final FeedRepository feedRepository;
+    private final MemberRepository memberRepository;
 
+    @Transactional
+    public UploadFeedResponse uploadFeedByMember(Long memberId, String content, List<ImageElement> images) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new BusinessException(MemberErrorInfo.NOT_FOUND));
+
+        try{
+            String imageUrl = objectMapper.writeValueAsString(images);
+            Feed feed = FeedMapper.supplyFeedEntityBy(member, content, imageUrl);
+            return FeedMapper.supplyFeedResponseBy(feedRepository.save(feed));
+        } catch (JsonProcessingException e) {
+            throw new BusinessException(FeedErrorInfo.FEED_IMAGES_TO_JSON_STRING_ERROR);
+        }
+    }
 }
