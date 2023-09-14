7c1c9a49041ead09c39e75453ecfdf99ad4154b6
hyuna <kook980316@gmail.com>
2023-09-14 10:44:36 +0900

    feat: 캐러셀 추가

---
+++ frontend/coffeeing/src/components/Carousel.tsx
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import Slider from 'react-slick';
+import { CapsuleCard } from './CapsuleCard';
+import eximg from 'assets/google_logo.png';
+import Next from 'assets/nextarrow.png';
+import Prev from 'assets/prevarrow.png';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+function NextArrow(props: any) {
+  const { className, onClick } = props;
+  return (
+    <img
+      src={Next}
+      className={className}
+      style={{ display: 'block', width: '40px', height: '40px', right: '0' }}
+      onClick={onClick}
+    />
+  );
+}
+
+function PrevArrow(props: any) {
+  const { className, onClick } = props;
+  return (
+    <img
+      src={Prev}
+      className={className}
+      style={{ display: 'block', width: '40px', height: '40px', left: '0' }}
+      onClick={onClick}
+    />
+  );
+}
+
+export default class Carousel extends Component {
+  render() {
+    const settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      initialSlide: 0,
+      nextArrow: <NextArrow />,
+      prevArrow: <PrevArrow />,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: false,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    };
+
+    const capsuleList = [
+      { name: '아르페지오', brand: '네스프레소', capsule_id: 1, imgLink: '/' },
+      { name: '니카라과', brand: '네스프레소', capsule_id: 2, imgLink: '/' },
+      { name: '코지', brand: '네스프레소', capsule_id: 3, imgLink: '/' },
+      { name: '인도네시아', brand: '네스프레소', capsule_id: 4, imgLink: '/' },
+      { name: '볼루토', brand: '네스프레소', capsule_id: 5, imgLink: '/' },
+      { name: '베네치아', brand: '네스프레소', capsule_id: 6, imgLink: '/' },
+      { name: '리반토', brand: '네스프레소', capsule_id: 7, imgLink: '/' },
+      { name: '리스트레토', brand: '네스프레소', capsule_id: 8, imgLink: '/' },
+    ];
+
+    return (
+      <Slider {...settings} className="w-320 flex justify-center pl-10">
+        {capsuleList.map((capsule) => (
+          <CapsuleCard
+            brand={capsule.brand}
+            capsule_id={capsule.capsule_id}
+            name={capsule.name}
+            imgLink={eximg}
+            key={capsule.name}
+          />
+        ))}
+      </Slider>
+    );
+  }
+}
