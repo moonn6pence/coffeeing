30efeca27e980febc3216da80f76a55d93c27fb5
Hanul Kim <hanulkim2000@naver.com>
2023-09-14 13:08:03 +0900

    feat: 추천 메인 페이지 추가

--- frontend/coffeeing/src/pages/RecMainPage.tsx
+++ frontend/coffeeing/src/pages/RecMainPage.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import recBgImg from '../assets/surveyMainImg.png'
 
-export const RecMainPage = () =>{
-  return(
-    <div>
-      <img className='w-screen ' src={recBgImg}/>
-    </div>  
+export const RecMainPage = () => {
+  const navitage = useNavigate();
+  return (
+    <div className='relative flex items-center justify-center w-screen h-screen'>
+      <img className='w-full h-full brightness-50' src={recBgImg} alt="배경 이미지" />
+      <div className='absolute flex flex-col items-center gap-10 top-1/3'>
+        <div className='flex flex-col items-center'>
+          <p className='text-4xl text-white z-10'>본인의 취향에 맞는</p>
+          <p className='text-4xl text-white z-10'>원두와 캡슐을 추천 받아보세요.</p>
+        </div>
+        <button 
+          className='bg-white text-black w-60 h-12'
+          onClick={()=>navitage('/recommend-survey')}
+          >시작하기</button>
+      </div>
+    </div>
   )
 }
