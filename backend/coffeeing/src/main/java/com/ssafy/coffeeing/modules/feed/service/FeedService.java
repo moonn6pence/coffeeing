66abd36000b254daf230b033e8d04b50f53668e0
taeyong <amico741@naver.com>
2023-09-14 16:11:32 +0900

    feat: Feed의 좋아요 토글 비즈니스 로직 추가

--- backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/feed/service/FeedService.java
+++ backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/feed/service/FeedService.java
@@ -3,11 +3,15 @@ package com.ssafy.coffeeing.modules.feed.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.coffeeing.modules.feed.domain.Feed;
+import com.ssafy.coffeeing.modules.feed.domain.FeedLike;
 import com.ssafy.coffeeing.modules.feed.dto.UpdateFeedRequest;
 import com.ssafy.coffeeing.modules.feed.dto.UploadFeedRequest;
 import com.ssafy.coffeeing.modules.feed.dto.UploadFeedResponse;
+import com.ssafy.coffeeing.modules.feed.mapper.FeedLikeMapper;
 import com.ssafy.coffeeing.modules.feed.mapper.FeedMapper;
+import com.ssafy.coffeeing.modules.feed.repository.FeedLikeRepository;
 import com.ssafy.coffeeing.modules.feed.repository.FeedRepository;
+import com.ssafy.coffeeing.modules.global.dto.ToggleResponse;
 import com.ssafy.coffeeing.modules.global.exception.BusinessException;
 import com.ssafy.coffeeing.modules.global.exception.info.FeedErrorInfo;
 import com.ssafy.coffeeing.modules.global.security.util.SecurityContextUtils;
@@ -16,11 +20,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class FeedService {
 
     private final FeedRepository feedRepository;
+    private final FeedLikeRepository feedLikeRepository;
     private final SecurityContextUtils securityContextUtils;
 
     @Transactional
@@ -55,5 +62,32 @@ public class FeedService {
                 .orElseThrow(() -> new BusinessException(FeedErrorInfo.NOT_FOUND));
 
         feed.updateContent(updateFeedRequest.content());
+    }
+
+    @Transactional
+    public ToggleResponse toggleFeedLike(Long feedId) {
+        Member member = securityContextUtils.getCurrnetAuthenticatedMember();
+        Feed feed = feedRepository.findById(feedId)
+                .orElseThrow(() -> new BusinessException(FeedErrorInfo.NOT_FOUND));
+
+        Optional<FeedLike> feedLike = feedLikeRepository.findFeedLikeByFeedAndMember(feed, member);
+
+        if(feedLike.isPresent()) {
+            feedLikeRepository.delete(feedLike.get());
+            return decreaseFeedLikeCount(feed);
+        } else {
+            feedLikeRepository.save(FeedLikeMapper.supplyFeedLikeEntityBy(feed, member));
+            return increaseFeedLikeCount(feed);
+        }
+    }
+
+    private ToggleResponse decreaseFeedLikeCount(Feed feed) {
+        feed.decreaseLikeCount();
+        return FeedLikeMapper.supplyFeedLikeResponseBy(false);
+    }
+
+    private ToggleResponse increaseFeedLikeCount(Feed feed) {
+        feed.increaseLikeCount();
+        return FeedLikeMapper.supplyFeedLikeResponseBy(true);
     }
 }
