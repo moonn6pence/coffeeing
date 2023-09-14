c43e48962f2c279786b4fa213c8195de3bbdf888
Hanul Kim <hanulkim2000@naver.com>
2023-09-14 12:29:02 +0900

    refactor: 설문 단계별로 component 다르게 보이게 변경

--- frontend/coffeeing/src/pages/RecSurveyPage.tsx
+++ frontend/coffeeing/src/pages/RecSurveyPage.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { RoastingSelect } from 'components/Survey/RoastingSelect';
+import { AciditySelect } from 'components/Survey/AciditySelect';
+import { RootState } from 'store/store';
 
 export const RecSurveyPage = () =>{
+  const survey = useSelector((state:RootState)=>state.survey)
+
   return(
     <div>
-      <RoastingSelect/>
+      {survey.currentPage==1&&
+        <RoastingSelect/>
+      }
+      {survey.currentPage==2&&
+        <AciditySelect/>
+      }
     </div>
   )
 }
