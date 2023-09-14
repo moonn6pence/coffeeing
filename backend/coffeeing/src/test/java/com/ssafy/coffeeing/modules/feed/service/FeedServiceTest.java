8788f46a4dbf9cd0088e8f29732ad3f32d4f0a54
taeyong <amico741@naver.com>
2023-09-14 15:11:19 +0900

    test: test에서 사용할 Member Instance 추가

--- backend/coffeeing/src/test/java/com/ssafy/coffeeing/modules/feed/service/FeedServiceTest.java
+++ backend/coffeeing/src/test/java/com/ssafy/coffeeing/modules/feed/service/FeedServiceTest.java
@@ -1,7 +1,6 @@
 package com.ssafy.coffeeing.modules.feed.service;
 
 import com.ssafy.coffeeing.dummy.FeedTestDummy;
-import com.ssafy.coffeeing.dummy.MemberTestDummy;
 import com.ssafy.coffeeing.modules.feed.domain.Feed;
 import com.ssafy.coffeeing.modules.feed.dto.UpdateFeedRequest;
 import com.ssafy.coffeeing.modules.feed.dto.UploadFeedRequest;
@@ -10,8 +9,6 @@ import com.ssafy.coffeeing.modules.feed.repository.FeedRepository;
 import com.ssafy.coffeeing.modules.global.exception.BusinessException;
 import com.ssafy.coffeeing.modules.global.exception.info.FeedErrorInfo;
 import com.ssafy.coffeeing.modules.global.security.util.SecurityContextUtils;
-import com.ssafy.coffeeing.modules.member.domain.Member;
-import com.ssafy.coffeeing.modules.member.repository.MemberRepository;
 import com.ssafy.coffeeing.modules.util.ServiceTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -33,9 +30,6 @@ class FeedServiceTest extends ServiceTest {
     @Autowired
     private FeedRepository feedRepository;
 
-    @Autowired
-    private MemberRepository memberRepository;
-
     @MockBean
     private SecurityContextUtils securityContextUtils;
 
@@ -43,10 +37,8 @@ class FeedServiceTest extends ServiceTest {
     @Test
     void Given_UploadFeedRequest_When_SaveFeed_Then_Success() {
         //given
-        Member member = memberRepository.save(MemberTestDummy
-                .createGeneralMember("testNickname", "test123", "test1@test.com"));
         given(securityContextUtils.getCurrnetAuthenticatedMember())
-                .willReturn(member);
+                .willReturn(generalMember);
         UploadFeedRequest uploadFeedRequest = FeedTestDummy.createUploadFeedRequest();
 
         //when
@@ -71,10 +63,8 @@ class FeedServiceTest extends ServiceTest {
     @Test
     void Given_DeleteFeedRequest_When_DeleteFeed_Then_Success() {
         //given
-        Member member = memberRepository.save(MemberTestDummy
-                .createGeneralMember("paul", "test", "test2@naver.com"));
-        Feed feed = feedRepository.save(FeedTestDummy.createFeed(member));
-        given(securityContextUtils.getCurrnetAuthenticatedMember()).willReturn(member);
+        Feed feed = feedRepository.save(FeedTestDummy.createFeed(generalMember));
+        given(securityContextUtils.getCurrnetAuthenticatedMember()).willReturn(generalMember);
 
         //when, then
         feedService.deleteFeedById(feed.getId());
@@ -87,10 +77,8 @@ class FeedServiceTest extends ServiceTest {
     @Test
     void Given_UpdateFeedRequest_When_UpdateFeed_Then_Success() {
         //given
-        Member member = memberRepository.save(MemberTestDummy
-                .createGeneralMember("testNickname", "123", "amico@naver.com"));
-        Feed feed = feedRepository.save(FeedTestDummy.createFeed(member));
-        given(securityContextUtils.getCurrnetAuthenticatedMember()).willReturn(member);
+        Feed feed = feedRepository.save(FeedTestDummy.createFeed(generalMember));
+        given(securityContextUtils.getCurrnetAuthenticatedMember()).willReturn(generalMember);
         UpdateFeedRequest updateFeedRequest = FeedTestDummy.createUpdateFeedRequest();
 
         //when
@@ -107,10 +95,8 @@ class FeedServiceTest extends ServiceTest {
     @Test
     void Given_UpdateFeed_With_InValidID_When_UpdateFeed_Then_Fail() {
         //given
-        Member member = memberRepository.save(MemberTestDummy
-                .createGeneralMember("paul", "test1", "amico1@test.com"));
-        Feed feed = feedRepository.save(FeedTestDummy.createFeed(member));
-        given(securityContextUtils.getCurrnetAuthenticatedMember()).willReturn(member);
+        Feed feed = feedRepository.save(FeedTestDummy.createFeed(generalMember));
+        given(securityContextUtils.getCurrnetAuthenticatedMember()).willReturn(generalMember);
         UpdateFeedRequest updateFeedRequest = FeedTestDummy.createUpdateFeedRequest();
 
         //when, then
@@ -125,14 +111,10 @@ class FeedServiceTest extends ServiceTest {
     @Test
     void Given_UpdateFeed_With_InValidMember_When_UpdateFeed_Then_Fail() {
         //given
-        Member member = memberRepository.save(MemberTestDummy
-                .createGeneralMember("james", "testPassword", "test1@naver.com"));
-        Member other = memberRepository.save(MemberTestDummy
-                .createBeforeResearchMember("paul", "testPassword", "zase@naver.com"));
-        Feed feed = feedRepository.save(FeedTestDummy.createFeed(member));
+        Feed feed = feedRepository.save(FeedTestDummy.createFeed(generalMember));
         UpdateFeedRequest updateFeedRequest = FeedTestDummy.createUpdateFeedRequest();
         given(securityContextUtils.getCurrnetAuthenticatedMember())
-                .willReturn(other);
+                .willReturn(beforeResearchMember);
 
         //when, then
         assertEquals(FeedErrorInfo.NOT_FOUND, assertThrows(BusinessException.class,
@@ -146,10 +128,8 @@ class FeedServiceTest extends ServiceTest {
     @Test
     void Given_DeleteRequest_When_DeleteFeed_Then_Fail() {
         //given
-        Member member = memberRepository.save(MemberTestDummy
-                .createGeneralMember("shawn", "testPassword", "test10@naver.com"));
-        Feed feed = feedRepository.save(FeedTestDummy.createFeed(member));
-        given(securityContextUtils.getCurrnetAuthenticatedMember()).willReturn(member);
+        Feed feed = feedRepository.save(FeedTestDummy.createFeed(generalMember));
+        given(securityContextUtils.getCurrnetAuthenticatedMember()).willReturn(beforeResearchMember);
 
         //when, then
         assertEquals(FeedErrorInfo.NOT_FOUND, assertThrows(BusinessException.class,
@@ -163,13 +143,9 @@ class FeedServiceTest extends ServiceTest {
     @Test
     void Given_DeleteRequest_With_InvalidMember_When_DeleteFeed_Then_Fail() {
         //given
-        Member member = memberRepository.save(MemberTestDummy
-                .createGeneralMember("shawnMendes", "srerwe", "test1@naver.com"));
-        Member other = memberRepository.save(MemberTestDummy
-                .createBeforeResearchMember("wony", "zserwe", "test2@naver.com"));
-        Feed feed = feedRepository.save(FeedTestDummy.createFeed(member));
+        Feed feed = feedRepository.save(FeedTestDummy.createFeed(generalMember));
         given(securityContextUtils.getCurrnetAuthenticatedMember())
-                .willReturn(other);
+                .willReturn(beforeResearchMember);
 
         //when, then
         assertEquals(FeedErrorInfo.NOT_FOUND, assertThrows(BusinessException.class,
