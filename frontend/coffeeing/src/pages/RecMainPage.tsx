bdb3ade7dd54a6e3603cf23e26ac14e017dd0dc7
Hanul Kim <hanulkim2000@naver.com>
2023-09-14 12:25:26 +0900

    feat: 추천 메인 페이지 추가

---
+++ frontend/coffeeing/src/pages/RecMainPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import recBgImg from '../assets/surveyMainImg.png'
+
+export const RecMainPage = () =>{
+  return(
+    <div>
+      <img className='w-screen ' src={recBgImg}/>
+    </div>  
+  )
+}
