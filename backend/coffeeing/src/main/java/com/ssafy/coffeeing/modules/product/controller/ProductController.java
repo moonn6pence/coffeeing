58ad6443ed7f2c67a7388161342972a6ece03a07
신현철 <shin421179@gmail.com>
2023-09-14 09:27:02 +0900

    feat : product review 관련 api 명세 보완

--- backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/product/controller/ProductController.java
+++ backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/product/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.ssafy.coffeeing.modules.product.controller;
 
+import com.ssafy.coffeeing.modules.global.dto.CreationResponse;
 import com.ssafy.coffeeing.modules.global.dto.ToggleResponse;
 import com.ssafy.coffeeing.modules.product.dto.*;
 import com.ssafy.coffeeing.modules.product.service.CapsuleReviewService;
@@ -79,4 +80,41 @@ public class ProductController {
                 .data(coffeeService.getSimilarCapsules(id))
                 .build();
     }
+
+    @PostMapping("/capsule/review/{id}")
+    public BaseResponse<CreationResponse> createCapsuleReview(@RequestBody ReviewRequest reviewRequest){
+
+        return BaseResponse.<CreationResponse>builder().build();
+    }
+
+    @PostMapping("/coffee/review/{id}")
+    public BaseResponse<CreationResponse> createCoffeeReview(@RequestBody ReviewRequest reviewRequest){
+
+        return BaseResponse.<CreationResponse>builder().build();
+    }
+
+    @PutMapping("/capsule/review/{id}")
+    public BaseResponse<Void> editCapsuleReview(@RequestBody ReviewRequest reviewRequest){
+
+        return BaseResponse.<Void>builder().build();
+    }
+
+    @PutMapping("/coffee/review/{id}")
+    public BaseResponse<Void> editCoffeeReview(@RequestBody ReviewRequest reviewRequest){
+
+        return BaseResponse.<Void>builder().build();
+    }
+
+    @DeleteMapping("/capsule/review/{id}")
+    public BaseResponse<Void> deleteCapsuleReview(@PathVariable @NumberFormat Long id){
+
+        return BaseResponse.<Void>builder().build();
+    }
+
+    @DeleteMapping("/coffee/review/{id}")
+    public BaseResponse<Void> deleteCoffeeReview(@PathVariable @NumberFormat Long id){
+
+        return BaseResponse.<Void>builder().build();
+    }
+
 }
