a3ee0073b24596110d6496bdde6d929e0998d91e
khs960616 <anjfkrhdkdle@naver.com>
2023-09-14 21:55:12 +0900

    fix: Service Test 더미 멤버 사용하도록 인증 서비스 테스트 수정

--- backend/coffeeing/src/test/java/com/ssafy/coffeeing/modules/auth/service/AuthServiceTest.java
+++ backend/coffeeing/src/test/java/com/ssafy/coffeeing/modules/auth/service/AuthServiceTest.java
@@ -6,9 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.ssafy.coffeeing.dummy.MemberTestDummy;
 import com.ssafy.coffeeing.modules.auth.dto.SignInRequest;
 import com.ssafy.coffeeing.modules.auth.dto.SignInResponse;
 import com.ssafy.coffeeing.modules.auth.dto.SignUpRequest;
@@ -16,7 +14,6 @@ import com.ssafy.coffeeing.modules.auth.dto.SignUpResponse;
 import com.ssafy.coffeeing.modules.global.exception.BusinessException;
 import com.ssafy.coffeeing.modules.global.exception.info.MemberErrorInfo;
 import com.ssafy.coffeeing.modules.member.domain.Member;
-import com.ssafy.coffeeing.modules.member.domain.MemberState;
 import com.ssafy.coffeeing.modules.member.repository.MemberRepository;
 import com.ssafy.coffeeing.modules.util.ServiceTest;
 
@@ -27,9 +24,6 @@ public class AuthServiceTest extends ServiceTest {
 
 	@Autowired
 	private AuthService authService;
-
-	@Autowired
-	private PasswordEncoder passwordEncoder;
 
 	private final String TEST_PASSWORD = "mklrgjanj3@$nDfsn";
 	private final String GRANT_TYPE = "Bearer";
@@ -59,9 +53,7 @@ public class AuthServiceTest extends ServiceTest {
 	@DisplayName("이미 가입되어있는 이메일로 회원가입 시 이메일 중복 예외를 발생시킨다.")
 	void Given_ValidEmailAndPassword_When_SignUp_Then_ThrowPreExistEmailException() {
 		//given
-		String preExistEmail = "kim@naver.com";
-		getTestPersistenceMember(null, TEST_PASSWORD, preExistEmail, MemberState.BEFORE_RESEARCH);
-
+		String preExistEmail = generalMember.getEmail();
 		SignUpRequest signUpRequest = new SignUpRequest(preExistEmail, TEST_PASSWORD);
 		//when, then
 		assertEquals(MemberErrorInfo.PRE_EXIST_EMAIL,
@@ -72,9 +64,7 @@ public class AuthServiceTest extends ServiceTest {
 	@DisplayName("이메일과 비밀번호를 입력하여 로그인을 진행한다.")
 	void Given_ValidEmailAndPassword_When_SignIn_Then_Success() {
 		//given
-		Member preSignUpMember = getTestPersistenceMember(null, TEST_PASSWORD, "t6est@naver.com",
-			MemberState.BEFORE_RESEARCH);
-		SignInRequest signInRequest = new SignInRequest(preSignUpMember.getEmail(), preSignUpMember.getPassword());
+		SignInRequest signInRequest = new SignInRequest(generalMember.getEmail(), generalMember.getPassword());
 		//when
 		SignInResponse signInResponse = authService.signIn(signInRequest.getMemberEmailAndPasswordAuthentication());
 		//then
@@ -83,11 +73,5 @@ public class AuthServiceTest extends ServiceTest {
 			() -> assertNotNull(signInResponse.refreshToken()),
 			() -> assertEquals(GRANT_TYPE, signInResponse.grantType())
 		);
-	}
-
-	private Member getTestPersistenceMember(String nickname, String rawPassword, String email, MemberState state) {
-		String encodePassword = passwordEncoder.encode(rawPassword);
-		Member member = MemberTestDummy.createMember(nickname, encodePassword, email, state);
-		return memberRepository.save(member);
 	}
 }
