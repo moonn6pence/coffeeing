6635394b3e30c1f087e52590dfd2f4541f4fa04f
moonn6pence <shin421179@gmail.com>
2023-09-15 03:05:40 +0900

    feat : 원두 더미, 원두 리뷰 더미 추가

---
+++ backend/coffeeing/src/main/java/com/ssafy/coffeeing/dummy/CoffeeDummy.java
@@ -0,0 +1,69 @@
+package com.ssafy.coffeeing.dummy;
+
+import com.ssafy.coffeeing.modules.global.embedded.CoffeeCriteria;
+import com.ssafy.coffeeing.modules.product.domain.Coffee;
+import com.ssafy.coffeeing.modules.product.repository.CoffeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Profile("dev")
+@RequiredArgsConstructor
+@Component
+public class CoffeeDummy {
+
+    private final List<String> coffeeNames = List.of("Kenya AA", "Kenya AB", "Geisha", "아르페지오", "로마");
+    private final List<Double> roasts = List.of(1.0, 1.0, 0.8, 0.8, 0.6);
+    private final List<Double> acidities = List.of(1.0, 1.0, 0.8, 0.8, 0.6);
+    private final List<Double> bodies = List.of(0.2, 0.2, 0.6, 0.4, 0.8);
+    private final List<String> aroma = List.of("roasted, intense dark roasted", "spicy, dark roasted, woody", "dark roasted", "intense, cocoa", "woody, grain");
+
+    private final List<String> imageUrls = List.of(
+            "https://sample.image1.png",
+            "https://sample.image2.png",
+            "https://sample.image3.png",
+            "https://sample.image4.png",
+            "https://sample.image5.png"
+    );
+    private final List<String> productDescriptions = List.of(
+            "sample description 1",
+            "sample description 2",
+            "sample description 3",
+            "sample description 4",
+            "sample description 5"
+    );
+    private final List<Double> totalScores = List.of(38.9, 48.3, 42.1, 49.8, 25.5);
+
+
+    private final CoffeeRepository coffeeRepository;
+
+    public List<Coffee> create5CoffeeDummy(){
+
+        List<Coffee> coffees = new ArrayList<>();
+
+        for (int i = 0; i < coffeeNames.size(); i++) {
+            coffees.add(createCoffee(coffeeNames.get(i), aroma.get(i),
+                    imageUrls.get(i), productDescriptions.get(i),
+                    new CoffeeCriteria(roasts.get(i), acidities.get(i), bodies.get(i)),
+                    totalScores.get(i), 5));
+        }
+
+        return coffeeRepository.saveAll(coffees);
+    }
+
+    private Coffee createCoffee(String coffeeName, String aroma, String imageUrl, String productDescription,
+                                CoffeeCriteria coffeeCriteria, Double totalScore ,Integer totalReviewer){
+        return Coffee.builder()
+                .coffeeName(coffeeName)
+                .coffeeCriteria(coffeeCriteria)
+                .aroma(aroma)
+                .imageUrl(imageUrl)
+                .productDescription(productDescription)
+                .totalScore(totalScore)
+                .totalReviewer(totalReviewer)
+                .build();
+    }
+}
--- backend/coffeeing/src/main/java/com/ssafy/coffeeing/dummy/Dummy.java
+++ backend/coffeeing/src/main/java/com/ssafy/coffeeing/dummy/Dummy.java
@@ -2,6 +2,7 @@ package com.ssafy.coffeeing.dummy;
 
 import com.ssafy.coffeeing.modules.member.domain.Member;
 import com.ssafy.coffeeing.modules.product.domain.Capsule;
+import com.ssafy.coffeeing.modules.product.domain.Coffee;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
@@ -21,10 +22,12 @@ public class Dummy implements CommandLineRunner {
 
     private final EntityManager em;
     private final CapsuleDummy capsuleDummy;
+    private final CoffeeDummy coffeeDummy;
     private final FeedDummy feedDummy;
     private final MemberDummy memberDummy;
 
     private final CapsuleReviewDummy capsuleReviewDummy;
+    private final CoffeeReviewDummy coffeeReviewDummy;
 
     private void flushAndClear(){
         em.flush();
@@ -38,9 +41,11 @@ public class Dummy implements CommandLineRunner {
         
         // dummy 객체 생성 및 저장
         List<Capsule> capsules = capsuleDummy.create5NespressoCapsules();
+        List<Coffee> coffees = coffeeDummy.create5CoffeeDummy();
         List<Member> members = memberDummy.createMemberDummies();
         feedDummy.createFeedDummies(members);
         capsuleReviewDummy.createCapsuleReviewOfCapsulesAndMembers(capsules, members);
+        coffeeReviewDummy.createCoffeeReviewOfCoffeesAndMembers(coffees, members);
 
         flushAndClear();
         
