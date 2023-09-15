97ed81c57f1dd4254e04dd5668a1d3fac0b6a620
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-09-15 16:53:36 +0900

    feat: Search Page 추가

---
+++ frontend/coffeeing/src/pages/SearchPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {SearchBar} from "components/Search/SearchBar";
+
+export const SearchPage = () =>{
+  
+  return(
+    <div>
+      <SearchBar/>
+    </div>
+  )
+}
