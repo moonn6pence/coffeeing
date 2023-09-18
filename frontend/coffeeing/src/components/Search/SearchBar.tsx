9e57e8f3bfb05ac9f9191ce1f2e7b78da939f37c
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-09-18 12:20:00 +0900

    feat: search bar searchSlice와 연결 추가

--- frontend/coffeeing/src/components/Search/SearchBar.tsx
+++ frontend/coffeeing/src/components/Search/SearchBar.tsx
@@ -1,14 +1,28 @@
-import React, {useState} from "react";
+import React, {ChangeEvent, useState} from "react";
+import searchIcon from '../../assets/search.png'
+import { useDispatch } from "react-redux";
+import { AppDispatch  } from 'store/store';
+import { setSearchText } from "store/searchSlice";
 
 export const SearchBar = ()=>{
+  const dispatch = useDispatch<AppDispatch>();
   const [title, setTitle] = useState('')
+  const handleTextChange = (e:ChangeEvent<HTMLInputElement>) =>{
+    setTitle(e.target.value)
+    dispatch(setSearchText(e.target.value))
+  }
   return(
-      <input 
-        className="border text-xl "
-        type="text" 
-        value={title} 
-        onChange={(e)=>setTitle(e.target.value)}
-        placeholder="검색어를 입력해주세요."
+      <div className="flex items-center border border-half-light w-790px h-16 rounded-md">
+        <span className="flex items-center pl-2">
+          <img className="w-5 h-5" src={searchIcon} alt="Search Icon" />
+        </span>
+        <input 
+          className="block py-4 pl-2 w-full focus:outline-none"
+          type="text" 
+          placeholder="검색어를 입력하세요."
+          value={title}
+          onChange={(e)=>handleTextChange(e)}
         />
+      </div>
   )
 }
