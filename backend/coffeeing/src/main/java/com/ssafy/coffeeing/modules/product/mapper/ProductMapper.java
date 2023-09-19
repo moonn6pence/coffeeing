f4139aa017925eadcd21478167bcfd00cd0641e0
백승윤 <seanbryan97@naver.com>
2023-09-19 13:40:24 +0900

    fix:simpleProductElement의 매개변수 순서 개정

--- backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/product/mapper/ProductMapper.java
+++ backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/product/mapper/ProductMapper.java
@@ -111,11 +111,11 @@ public class ProductMapper {
 
     public static SimpleProductElement supplySimpleProductElementOf(
             Long id,
-            String name,
             String subtitle, // input Region(Coffee) or Brand(Capsule)
+            String name,
             String imageUrl
     ) {
-        return new SimpleProductElement(id, name, subtitle, imageUrl);
+        return new SimpleProductElement(id, subtitle, name, imageUrl);
     }
 
     public static BookmarkedResponse supplyBookmarkedResponseOf(Page<SimpleProductElement> bookmarkedElements, boolean isCapsule) {
