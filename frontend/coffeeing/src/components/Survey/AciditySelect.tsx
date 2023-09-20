5bcabd5cb5241779404eef4923123de5f79eec53
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-09-20 13:59:12 +0900

    refactor: 산미 정도 수정

--- frontend/coffeeing/src/components/Survey/AciditySelect.tsx
+++ frontend/coffeeing/src/components/Survey/AciditySelect.tsx
@@ -19,10 +19,10 @@ export const AciditySelect = () =>{
 
   // 데이터
   const data = [
-    {src:noAcid, label:'없음', isSelected:selectedNoAcid, setIsSelected:setSelectedNoAcid,num:0},
-    {src:lowAcid, label:'없음', isSelected:selectedLowAcid, setIsSelected:setSelectedLowAcid,num:0.4},
-    {src:mediumAcid, label:'없음', isSelected:selectedMediumAcid, setIsSelected:setSelectedMediumAcid,num:0.6},
-    {src:highAcid, label:'없음', isSelected:selectedHighAcid, setIsSelected:setSelectedHighAcid,num:0.8},
+    {src:noAcid, label:'없음', isSelected:selectedNoAcid, setIsSelected:setSelectedNoAcid,num:0.25},
+    {src:lowAcid, label:'낮음', isSelected:selectedLowAcid, setIsSelected:setSelectedLowAcid,num:0.5},
+    {src:mediumAcid, label:'중간', isSelected:selectedMediumAcid, setIsSelected:setSelectedMediumAcid,num:0.75},
+    {src:highAcid, label:'높음', isSelected:selectedHighAcid, setIsSelected:setSelectedHighAcid,num:1},
   ]
   // 산미 단계 이미지 클릭 시
   const handleAciditySelect = (num:number,isSelected:boolean,setIsSelected:Dispatch<SetStateAction<boolean>>)=>{
@@ -40,7 +40,7 @@ export const AciditySelect = () =>{
   const handleAciditySubmit = ()=>{
     // 선택 안 했을 때
     if (myAcidity===-1) {
-      alert('선호하는 로스팅 단계를 하나 선택해주세요')
+      alert('선호하는 산미 단계를 하나 선택해주세요')
     } 
     // 선택 했을 때 - 다음 페이지로 & 산미 정보 저장
     else {
@@ -49,7 +49,7 @@ export const AciditySelect = () =>{
     }
   }
   return(
-    <div className='flex flex-col items-center gap-10'>
+    <div className='flex flex-col items-center gap-10 mt-10'>
       {/* 설문 상단 */}
       <div className='flex flex-col items-center gap-2'>
         <p>{survey.currentPage}/{survey.totalPage}</p>
