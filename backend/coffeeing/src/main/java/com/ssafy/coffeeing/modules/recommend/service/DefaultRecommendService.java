8293578967158610558446c3d72252aa6089867c
신현철 <shin421179@gmail.com>
2023-09-20 17:11:04 +0900

    feat : 추천 모델 예외 처리 추가

--- backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/recommend/service/DefaultRecommendService.java
+++ backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/recommend/service/DefaultRecommendService.java
@@ -1,17 +1,21 @@
 package com.ssafy.coffeeing.modules.recommend.service;
 
+import com.ssafy.coffeeing.modules.global.exception.BusinessException;
+import com.ssafy.coffeeing.modules.global.exception.info.SurveyErrorInfo;
 import com.ssafy.coffeeing.modules.recommend.dto.RecommendResponse;
 import com.ssafy.coffeeing.modules.survey.dto.PreferenceRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 import java.util.Optional;
 
-@Profile({"prod"})
+@Profile({"prod", "dev"})
 @RequiredArgsConstructor
 @Service
 public class DefaultRecommendService implements RecommendService {
@@ -37,7 +41,13 @@ public class DefaultRecommendService implements RecommendService {
                 .build()
                 .toUri();
 
-        return restTemplate.getForObject(uri, RecommendResponse.class);
+        try {
+            return restTemplate.getForObject(uri, RecommendResponse.class);
+        } catch (HttpClientErrorException e) {
+            throw new BusinessException(SurveyErrorInfo.BAD_API_SERVER_REQUEST);
+        } catch (HttpServerErrorException e) {
+            throw new BusinessException(SurveyErrorInfo.EXTERNAL_SERVER_ERROR);
+        }
     }
 
     @Override
@@ -51,6 +61,12 @@ public class DefaultRecommendService implements RecommendService {
                 .build()
                 .toUri();
 
-        return restTemplate.getForObject(uri, RecommendResponse.class);
+        try {
+            return restTemplate.getForObject(uri, RecommendResponse.class);
+        } catch (HttpClientErrorException e) {
+            throw new BusinessException(SurveyErrorInfo.BAD_API_SERVER_REQUEST);
+        } catch (HttpServerErrorException e) {
+            throw new BusinessException(SurveyErrorInfo.EXTERNAL_SERVER_ERROR);
+        }
     }
 }
