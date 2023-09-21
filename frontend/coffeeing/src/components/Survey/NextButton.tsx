42c32dba8f5b6d858c826f0200f5f1eaf4249e74
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-09-21 14:57:10 +0900

    design: 버튼 색깔 수정

--- frontend/coffeeing/src/components/Survey/NextButton.tsx
+++ frontend/coffeeing/src/components/Survey/NextButton.tsx
@@ -8,7 +8,7 @@ export const NextButton = ({handleClick}:NextButtonProps)=>{
   return(
 
       <button 
-        className='w-40 h-12 text-white bg-light-roasting text-xl'
+        className='w-40 h-12 text-white font-bold rounded-3xl bg-half-light text-xl'
         onClick={()=>handleClick()}
         >다음</button>
 
