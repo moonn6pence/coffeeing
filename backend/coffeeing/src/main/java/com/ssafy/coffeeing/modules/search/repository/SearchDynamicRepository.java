3e88776392eb05f1c144ebf875bc7863f4ed9bff
김태용 <amico741@naver.com>
2023-09-22 02:12:04 +0900

    refactor:  검색 조건 필터링에 대한 BooleanExpression 활용

--- backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/search/repository/SearchDynamicRepository.java
+++ backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/search/repository/SearchDynamicRepository.java
@@ -1,18 +1,24 @@
 package com.ssafy.coffeeing.modules.search.repository;
 
-import com.ssafy.coffeeing.modules.product.domain.Capsule;
-import com.ssafy.coffeeing.modules.product.domain.Coffee;
 import com.ssafy.coffeeing.modules.search.domain.Acidity;
 import com.ssafy.coffeeing.modules.search.domain.Body;
 import com.ssafy.coffeeing.modules.search.domain.Roast;
-import org.springframework.data.domain.Slice;
+import com.ssafy.coffeeing.modules.search.dto.ProductSearchElement;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface SearchDynamicRepository {
-    Slice<Coffee> searchByBeanConditions(List<Roast> roasts, List<Acidity> acidities, List<Body> bodies, String flavorNote,
-                                         Integer page, Integer size);
+    Page<ProductSearchElement> searchByBeanConditions(List<Roast> roasts,
+                                                      List<Acidity> acidities,
+                                                      List<Body> bodies,
+                                                      String flavorNote,
+                                                      Pageable pageable);
 
-    Slice<Capsule> searchByCapsuleConditions(List<Roast> roasts, List<Acidity> acidities, List<Body> bodies, String flavorNote,
-                                             Integer page, Integer size);
+    Page<ProductSearchElement> searchByCapsuleConditions(List<Roast> roasts,
+                                            List<Acidity> acidities,
+                                            List<Body> bodies,
+                                            String flavorNote,
+                                            Pageable pageable);
 }
--- backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/search/repository/SearchDynamicRepositoryImpl.java
+++ backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/search/repository/SearchDynamicRepositoryImpl.java
@@ -1,24 +1,22 @@
 package com.ssafy.coffeeing.modules.search.repository;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.ssafy.coffeeing.modules.global.embedded.QCoffeeCriteria;
-import com.ssafy.coffeeing.modules.product.domain.Capsule;
-import com.ssafy.coffeeing.modules.product.domain.Coffee;
 import com.ssafy.coffeeing.modules.search.domain.Acidity;
 import com.ssafy.coffeeing.modules.search.domain.Body;
 import com.ssafy.coffeeing.modules.search.domain.Roast;
+import com.ssafy.coffeeing.modules.search.dto.ProductSearchElement;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Slice;
-import org.springframework.data.domain.SliceImpl;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
 
-import static com.ssafy.coffeeing.modules.global.embedded.QCoffeeCriteria.*;
+import static com.ssafy.coffeeing.modules.global.embedded.QCoffeeCriteria.coffeeCriteria;
 import static com.ssafy.coffeeing.modules.product.domain.QCapsule.capsule;
 import static com.ssafy.coffeeing.modules.product.domain.QCoffee.coffee;
 
@@ -29,41 +27,73 @@ public class SearchDynamicRepositoryImpl implements SearchDynamicRepository {
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
-    public Slice<Coffee> searchByBeanConditions(
+    public Page<ProductSearchElement> searchByBeanConditions(
             List<Roast> roasts,
             List<Acidity> acidities,
             List<Body> bodies,
             String flavorNote,
-            Integer page,
-            Integer size) {
+            Pageable pageable) {
 
-        List<Coffee> coffees = jpaQueryFactory.selectFrom(coffee)
-                .where(makeConditionsOfRoastWithAcidityWithBody(roasts, acidities, bodies))
-                .where(coffee.flavorNote.contains(flavorNote))
-                .offset(page)
-                .limit(size)
-                .fetch();
+        List<ProductSearchElement> coffees = jpaQueryFactory
+                .select(Projections.fields(ProductSearchElement.class,
+                        coffee.id.as("id"), coffee.coffeeName.as("name"), coffee.imageUrl.as("imageUrl")))
+                .from(coffee)
+                .where(makeConditionsOfRoastWithAcidityWithBody(roasts, acidities, bodies)
+                        .or(containsCoffeeFlavorNote(flavorNote)))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .stream().distinct().toList();
 
-        return null;
+        Long count = jpaQueryFactory
+                .select(coffee.count())
+                .from(coffee)
+                .where(makeConditionsOfRoastWithAcidityWithBody(roasts, acidities, bodies)
+                        .or(containsCoffeeFlavorNote(flavorNote)))
+                .fetchOne();
+
+        return new PageImpl<>(coffees, pageable, count);
     }
 
     @Override
-    public Slice<Capsule> searchByCapsuleConditions(
+    public Page<ProductSearchElement> searchByCapsuleConditions(
             List<Roast> roasts,
             List<Acidity> acidities,
             List<Body> bodies,
             String flavorNote,
-            Integer page,
-            Integer size) {
+            Pageable pageable) {
 
-        List<Capsule> capsules = jpaQueryFactory.selectFrom(capsule)
-                .where(makeConditionsOfRoastWithAcidityWithBody(roasts, acidities, bodies))
-                .where(capsule.flavorNote.contains(flavorNote))
-                .offset(page)
-                .limit(size)
-                .fetch();
+        List<ProductSearchElement> capsules = jpaQueryFactory
+                .select(Projections.fields(ProductSearchElement.class,
+                        capsule.id.as("id"), capsule.capsuleName.as("name"), capsule.imageUrl.as("imageUrl")))
+                .from(capsule)
+                .where(makeConditionsOfRoastWithAcidityWithBody(roasts, acidities, bodies)
+                        .or(containsCapsuleFlavorNote(flavorNote)))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .stream().distinct().toList();
 
-        return new SliceImpl<>(capsules, );
+        Long count = jpaQueryFactory
+                .select(capsule.count())
+                .from(capsule)
+                .where(makeConditionsOfRoastWithAcidityWithBody(roasts, acidities, bodies)
+                        .or(containsCapsuleFlavorNote(flavorNote)))
+                .fetchOne();
+
+        return new PageImpl<>(capsules, pageable, count);
+    }
+
+    private BooleanExpression containsCoffeeFlavorNote(String flavorNote) {
+        if(flavorNote == null) {
+            return null;
+        }
+        return coffee.flavorNote.containsIgnoreCase(flavorNote);
+    }
+
+    private BooleanExpression containsCapsuleFlavorNote(String flavorNote) {
+        if (flavorNote == null) {
+            return null;
+        }
+        return capsule.flavorNote.containsIgnoreCase(flavorNote);
     }
 
     private BooleanBuilder makeConditionsOfRoastWithAcidityWithBody(
@@ -72,20 +102,14 @@ public class SearchDynamicRepositoryImpl implements SearchDynamicRepository {
             List<Body> bodies) {
         BooleanBuilder booleanBuilder = new BooleanBuilder();
 
-        roasts.forEach(roast -> {
-            booleanBuilder.and(coffeeCriteria.roast.gt(roast.getMinValue())
-                    .and(coffeeCriteria.roast.loe(roast.getMaxValue())));
-        });
+        roasts.forEach(roast -> booleanBuilder.or(coffeeCriteria.roast.gt(roast.getMinValue())
+                .and(coffeeCriteria.roast.loe(roast.getMaxValue()))));
 
-        acidities.forEach(acidity -> {
-            booleanBuilder.and(coffeeCriteria.acidity.gt(acidity.getMinValue())
-                    .and(coffeeCriteria.acidity.loe(acidity.getMaxValue())));
-        });
+        acidities.forEach(acidity -> booleanBuilder.or(coffeeCriteria.acidity.gt(acidity.getMinValue())
+                .and(coffeeCriteria.acidity.loe(acidity.getMaxValue()))));
 
-        bodies.forEach(body -> {
-            booleanBuilder.and(coffeeCriteria.body.gt(body.getMinValue())
-                    .and(coffeeCriteria.body.loe(body.getMaxValue())));
-        });
+        bodies.forEach(body -> booleanBuilder.or(coffeeCriteria.body.gt(body.getMinValue())
+                .and(coffeeCriteria.body.loe(body.getMaxValue()))));
 
         return booleanBuilder;
     }
