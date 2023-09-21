16d265e6ae4238f98f4f26ce84ae6934dd456b01
김태용 <amico741@naver.com>
2023-09-22 02:09:01 +0900

    feat: 검색 비즈니스 로직 예외 메시지 추가

--- backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/global/exception/info/SearchErrorInfo.java
+++ backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/global/exception/info/SearchErrorInfo.java
@@ -9,7 +9,8 @@ public enum SearchErrorInfo implements ErrorInfo {
 
     NOT_EXIST_ROAST("1100", "ROAST 정보를 찾을 수 없습니다"),
     NOT_EXIST_BODY("1101", "BODY 정보를 찾을 수 없습니다"),
-    NOT_EXIST_ACIDITY("1102", "ACIDITY 정보를 찾을 수 없습니다");
+    NOT_EXIST_ACIDITY("1102", "ACIDITY 정보를 찾을 수 없습니다"),
+    NOT_EXIST_TAG_TYPE("1103", "존재하지 않는 커피 타입입니다.");
 
     private final String code;
     private final String message;
