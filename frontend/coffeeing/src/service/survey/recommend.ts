794dd622875f223ff38806ae69f0e545cb106ce9
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-09-22 17:50:36 +0900

    feat: 선호 저장  api 작성

--- frontend/coffeeing/src/service/survey/recommend.ts
+++ frontend/coffeeing/src/service/survey/recommend.ts
@@ -5,6 +5,7 @@ import { API_URL } from "util/constants";
 import { surveyResult, mySurvey } from "./types";
 
 type MyRecommendationApiData = ApiSuccessResponse<surveyResult>
+type savePreferenceApiData = ApiSuccessResponse<null>
 
 export const getSurveyResult = (mySurvey:mySurvey):Promise<void|surveyResult>=> {
   return privateRequest
@@ -15,6 +16,19 @@ export const getSurveyResult = (mySurvey:mySurvey):Promise<void|surveyResult>=> 
   .catch((error)=>{
     if (!isAxiosError(error)) {
       console.error('[recommendation request fail]')
+    }
+  })
+}
+
+export const savePreference = (mySurvey:mySurvey):Promise<void|null>=>{
+  return privateRequest
+  .post<savePreferenceApiData>(`${API_URL}/survey/save`, mySurvey)
+  .then((res)=>{
+    return Promise.resolve(res.data.data)
+  })
+  .catch((error)=>{
+    if (!isAxiosError(error)) {
+      console.error('[save mypreference request fail]')
     }
   })
 }
