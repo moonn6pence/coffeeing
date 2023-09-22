9841f88ec686a1da94e9771142336ad5de49cb33
hyuna <kook980316@gmail.com>
2023-09-22 10:12:23 +0900

    feat: 버튼 어두운 버전 추가

--- frontend/coffeeing/src/components/NavBar/NavBarButton.tsx
+++ frontend/coffeeing/src/components/NavBar/NavBarButton.tsx
@@ -1,32 +1,42 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useDispatch } from "react-redux";
-import { AppDispatch  } from 'store/store';
-import { logout } from "store/memberSlice";
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from 'store/store';
+import { logout } from 'store/memberSlice';
 
 interface INavButtonProps {
   value: string;
   navLink: string;
-  isLogout?: boolean
+  isLogout?: boolean;
+  dark?: boolean;
 }
 
-export const NavBarButton = ({ value, navLink, isLogout=false }: INavButtonProps) => {
+export const NavBarButton = ({
+  value,
+  navLink,
+  isLogout = false,
+  dark = false,
+}: INavButtonProps) => {
   const dispatch = useDispatch<AppDispatch>();
   const navigate = useNavigate();
   const goLink = () => {
-    if(isLogout) {
-      // TODO LOGOUT API 
+    if (isLogout) {
+      // TODO LOGOUT API
       dispatch(logout());
     }
     navigate(`${navLink}`);
   };
 
+  const divCommon =
+    'px-22px py-3 rounded-3xl hover:brightness-90 cursor-pointer w-fit';
+  const pCommon = 'text-base text-light-roasting font-bold';
+
   return (
     <div
-      className="px-22px py-3 rounded-3xl bg-light hover:brightness-90 cursor-pointer"
+      className={dark ? `${divCommon} bg-my-black` : `${divCommon} bg-light`}
       onClick={goLink}
     >
-      <p className="text-base text-light-roasting font-bold">{value}</p>
+      <p className={dark ? `${pCommon} text-white` : `${pCommon}`}>{value}</p>
     </div>
   );
 };
