16e49a3d8dfff43f17034ffc6cdc0b203aa88eb9
백승윤 <seanbryan97@naver.com>
2023-09-22 15:34:17 +0900

    feat: awss3controller 추가

---
+++ backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/aws/controller/AwsS3Controller.java
@@ -0,0 +1,24 @@
+package com.ssafy.coffeeing.modules.aws.controller;
+
+import com.ssafy.coffeeing.modules.feed.dto.PresignedUrlResponse;
+import com.ssafy.coffeeing.modules.feed.service.AWSS3Service;
+import com.ssafy.coffeeing.modules.util.base.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/aws")
+public class AwsS3Controller {
+    private final AWSS3Service awsS3Service;
+
+    @GetMapping("/img")
+    public BaseResponse<PresignedUrlResponse> getImageUrl(){
+        return BaseResponse.<PresignedUrlResponse>builder()
+                .data(awsS3Service.getPresignedUrlWithImagePath())
+                .build();
+    }
+
+}
