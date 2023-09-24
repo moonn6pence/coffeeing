36ee6324256be0e85294a3de24afa81bc5d4f0c4
hanulkimm <hanulkim2000@naver.com>
2023-09-24 15:07:33 +0900

    feat: 제출 버튼 추가

--- frontend/coffeeing/src/components/Survey/MachineSelect.tsx
+++ frontend/coffeeing/src/components/Survey/MachineSelect.tsx
@@ -2,13 +2,16 @@ import nespresso from '../../assets/survey/machine/nespresso-removebg-preview.pn
 import nespresso_burtuo from '../../assets/survey/machine/nespresso_b-removebg-preview.png'
 import dolce from '../../assets/survey/machine/dolce-removebg-preview.png'
 import illi from '../../assets/survey/machine/illi-removebg-preview.png'
-import unknown from '../../assets/survey/machine/image_49-removebg-preview.png'
-
+import balmuda from '../../assets/survey/machine/balmuda.png'
 import React,{Dispatch, SetStateAction, useState}  from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import { AppDispatch, RootState } from 'store/store';
+import { saveMachineType } from 'store/surveySlice'
+import { useNavigate } from 'react-router-dom'
+import { NextButton } from './SurveyButton'
 
 export const MachineSelect = () => {
+  const navigate = useNavigate();
   const survey = useSelector((state:RootState)=>state.survey)
   const widthClass = `w-${survey.currentPage}/${survey.totalPage}`;
   const [selectedNespresso, setSelectedNespresso] = useState(false);
@@ -24,7 +27,7 @@ export const MachineSelect = () => {
     {src:nespresso_burtuo, label:'네스프레소 버추오',isSelected:selectedNespressoB, setIsSelected:setSelectedNespressoB, num:2},
     {src:dolce, label:'돌체구스토',isSelected:selectedDolce, setIsSelected:setSelectedDolce, num:3},
     {src:illi, label:'네스프레소',isSelected:selectedIlli, setIsSelected:setSelectedIlli, num:4},
-    {src:unknown, label:'기타', isSelected:selectedUnknown,setIsSelected:setSelectedUnknown, num:5}
+    {src:balmuda, label:'기타', isSelected:selectedUnknown,setIsSelected:setSelectedUnknown, num:5}
   ]
 
   const handleMachineSelect = (num:number, isSelected:boolean, setIsSelected:Dispatch<SetStateAction<boolean>>)=>{
@@ -39,18 +42,23 @@ export const MachineSelect = () => {
     }
   }
 
+  const handleSurveySubmit = ()=>{
+    dispatch(saveMachineType(myMachine))  
+    navigate('/recommend-result')
+  }
+
   return(
     <div className='flex flex-col items-center gap-5 mt-10'>
       {/* 설문 상단 */}
       <div className='flex flex-col items-center gap-2'>
         <p>{survey.currentPage}/{survey.totalPage}</p>
-        <p className='font-bold'>커피 머신 선택</p>
+        <p className='text-2xl font-bold'>사용 중인 커피 머신을 선택해주세요</p>
         <p className='relative w-560px h-2.5 rounded-lg bg-process-bar'>
           <p className={`absolute botton-0 left-0 ${widthClass} h-2.5 rounded-lg bg-light-roasting`}></p>
         </p>
       </div>
       {/* 설문 사진 */}
-      <div className='flex flex-row gap-10'>
+      <div className='flex flex-row gap-3'>
         {
           data.map((item) => {
             const { src, label, isSelected, setIsSelected, num } = item;
@@ -67,6 +75,8 @@ export const MachineSelect = () => {
           })
         }
       </div>
+      {/* 버튼 */}
+      <NextButton handleClick={handleSurveySubmit} label='제출하기'/>
     </div>
   )
 }
