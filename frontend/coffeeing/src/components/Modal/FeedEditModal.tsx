8d3fae7fc8d8acf8a812714ae7d08818111f117a
khs960616 <anjfkrhdkdle@naver.com>
2023-09-25 02:38:29 +0900

    feat: feed modal image upload drop & drop

--- frontend/coffeeing/src/components/Modal/FeedEditModal.tsx
+++ frontend/coffeeing/src/components/Modal/FeedEditModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Fragment, useRef, useState, useCallback, useEffect } from 'react'
+import { Fragment, useRef, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import UploadImageIcon from "assets/upload-img-icon.svg"
 import QuitModalIcon from "assets/quit-modal-icon.svg"
@@ -14,65 +14,33 @@ export const FeedEditModal = ({ isOpen, setIsOpen }:FeedEditModalProps) => {
 
   const cancelButtonRef = useRef(null);
   const [step, setStep] = useState<number>(1);
-  const [isDragging, setIsDragging] = useState<boolean>(false);
-  const dragRef = useRef<HTMLDivElement | null>(null);
+  const [isActive, setActive] = useState<boolean>(false);
+  const [uploadImage, setUploadImage] = useState<File>();
+  // const [preview, setPreview] = useState<any>();
 
-  const handleDragIn = useCallback((e: DragEvent): void => {
-    e.preventDefault();
-    e.stopPropagation();
-  }, []);
+  const handleDragStart = () => setActive(true);
+  const handleDragEnd = () => setActive(false);
+  const handleDragOver = (event: any) => {
+    event.preventDefault();
+  };
+  const handleDrop = (event: any) => {
+    event.preventDefault(); 
+    setUploadImage(event.dataTransfer.files[0]);
+    setStep(2);
+    setActive(false);
+  };
 
-  const handleDragOut = useCallback((e: DragEvent): void => {
-    e.preventDefault();
-    e.stopPropagation();
-
-    setIsDragging(false);
-  }, []);
-
-  const handleDragOver = useCallback((e: DragEvent): void => {
-    e.preventDefault();
-    e.stopPropagation();
-
-    if (e.dataTransfer!.files) {
-      setIsDragging(true);
-    }
-  }, []);
-
-  const handleDrop = useCallback(
-    (e: DragEvent): void => {
-      e.preventDefault();
-      e.stopPropagation();
-
-      setIsDragging(false);
-  }, []);
-
-  const initDragEvents = useCallback((): void => {
-    if (dragRef.current !== null) {
-      console.log(dragRef)
-      dragRef.current.addEventListener("dragenter", handleDragIn);
-      dragRef.current.addEventListener("dragleave", handleDragOut);
-      dragRef.current.addEventListener("dragover", handleDragOver);
-      dragRef.current.addEventListener("drop", handleDrop);
-    }
-  }, [handleDragIn, handleDragOut, handleDragOver, handleDrop]);
-
-  const resetDragEvents = useCallback((): void => {
-    if (dragRef.current !== null) {
-      dragRef.current.removeEventListener("dragenter", handleDragIn);
-      dragRef.current.removeEventListener("dragleave", handleDragOut);
-      dragRef.current.removeEventListener("dragover", handleDragOver);
-      dragRef.current.removeEventListener("drop", handleDrop);
-    }
-  }, [handleDragIn, handleDragOut, handleDragOver, handleDrop]);
-
-  useEffect(() => {
-    initDragEvents();
-    return () => resetDragEvents();
-  }, [initDragEvents, resetDragEvents]);
-
+  const handleUpload = (event: any) => {
+    setUploadImage(event.target.files[0]);
+    setStep(2);
+  };
+  
   return (
     <Transition.Root show={isOpen} as={Fragment}>
-      <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setIsOpen}>
+      <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={()=>{
+        setIsOpen(false);
+        setStep(1);
+      }}>
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
@@ -82,7 +50,7 @@ export const FeedEditModal = ({ isOpen, setIsOpen }:FeedEditModalProps) => {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+         <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
@@ -105,30 +73,56 @@ export const FeedEditModal = ({ isOpen, setIsOpen }:FeedEditModalProps) => {
                               <div className="grow">
                                 새 피드 만들기
                               </div>
-                              <div className="flex-none cursor-pointer" onClick={()=>setIsOpen(false)}>
+                              <div className="flex-none cursor-pointer" onClick={() => {
+                                    setIsOpen(false);
+                                    setStep(1);
+                              }}>
                                 <img src={QuitModalIcon} />
                               </div>
                             </div>
                        </Dialog.Title>
 
+           
                       <div className="mt-2 border-b border-gray-200"></div>
-                      {/** 이미지 드롭 다운 */}
-                      <div className={classNames(
-                        isDragging ? "bg-gray-200" : "",
-                        "drop-down-uploader mt-2 flex flex-col w-560px items-center aspect-square justify-center")} ref={dragRef}>
-                        <img src={UploadImageIcon} />
-                        <p className="text-xl text-gray-500 mt-4">
-                          사진을 여기에 끌어다 놓으세요
-                        </p>
-                        
-                        <label htmlFor="imageInput">
-                          <div className="w-fit justify-center mt-6 px-12 py-3  rounded-md bg-light-roasting text-sm font-semibold text-white shadow-sm hover:bg-cinamon-roasting">
-                                컴퓨터에서 선택
+                      { (step === 1) ? 
+                        <div className={
+                          classNames(
+                            isActive ? 'bg-gray-200' : '',
+                            "mt-2 flex flex-col w-560px items-center aspect-square justify-center"
+                          )}
+                          onDragEnter={ handleDragStart }
+                          onDragLeave={ handleDragEnd }
+                          onDragOver={ handleDragOver }  
+                          onDrop={ handleDrop }
+                          >
+                          <img src={UploadImageIcon} />
+                          <p className="text-xl text-gray-500 mt-4">
+                            사진을 여기에 끌어다 놓으세요
+                          </p>
+                          
+                          <label htmlFor="imageInput">
+                            <div className="w-fit justify-center mt-6 px-12 py-3  rounded-md bg-light-roasting text-sm font-semibold text-white shadow-sm hover:bg-cinamon-roasting">
+                                  컴퓨터에서 선택
+                            </div>
+                          </label>
+                          <input id="imageInput" className="hidden" type="file" accept="image/*" onChange={handleUpload}/>
+                        </div> :   
+                        <Transition.Child
+                          as={Fragment}
+                          enter="ease-out duration-300"
+                          enterFrom="opacity-0 translate-y-0"
+                          enterTo="opacity-100 translate-y-0"
+                          leave="ease-in duration-200"
+                          leaveFrom="opacity-100 translate-y-0"
+                          leaveTo="opacity-0 translate-y-4">
+
+                          <div className="w-560px h-30">
+                            {/* <img src={preview} /> */}
                           </div>
-                        </label>
-                        <input id="imageInput" className="hidden" type="file" accept="image/*"/>
+                        
+                        </Transition.Child>
+                      }
                       </div>
-                    </div>
                   </div>
                 </div>
               </Dialog.Panel>
