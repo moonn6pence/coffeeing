fb182214bfc2d781311ff04ab6cbfc8c5066caee
taeyong <amico741@naver.com>
2023-09-25 13:52:48 +0900

    docs: feed domain api operations 추가

--- backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/feed/controller/FeedController.java
+++ backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/feed/controller/FeedController.java
@@ -6,6 +6,7 @@ import com.ssafy.coffeeing.modules.global.dto.ToggleResponse;
 import com.ssafy.coffeeing.modules.util.base.BaseResponse;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.NumberFormat;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +21,7 @@ public class FeedController {
     private final FeedService feedService;
 
     @PostMapping
+    @ApiOperation(value = "피드 업로드")
     public BaseResponse<UploadFeedResponse> uploadFeedRequest(@Valid @RequestBody UploadFeedRequest uploadFeedRequest) {
         return BaseResponse.<UploadFeedResponse>builder()
                 .data(feedService.uploadFeedByMember(uploadFeedRequest))
@@ -27,6 +29,7 @@ public class FeedController {
     }
 
     @PatchMapping("/{feedId}")
+    @ApiOperation(value = "피드 콘텐츠 업데이트 요청")
     public BaseResponse<Void> updateFeedContent(
             @PathVariable @NumberFormat Long feedId,
             @Valid @RequestBody UpdateFeedRequest updateFeedRequest) {
@@ -36,6 +39,7 @@ public class FeedController {
     }
 
     @DeleteMapping("/{feedId}")
+    @ApiOperation(value = "피드 삭제")
     public BaseResponse<Void> deleteFeed(@PathVariable @NumberFormat Long feedId) {
         feedService.deleteFeedById(feedId);
         return BaseResponse.<Void>builder()
@@ -43,6 +47,7 @@ public class FeedController {
     }
 
     @PostMapping("/{feedId}/like")
+    @ApiOperation(value = "피드 좋아요 토글")
     public BaseResponse<ToggleResponse> toggleFeedLike(@PathVariable @NumberFormat Long feedId) {
         return BaseResponse.<ToggleResponse>builder()
                 .data(feedService.toggleFeedLike(feedId))
@@ -71,6 +76,7 @@ public class FeedController {
                     )
             })
     @GetMapping("/my-list")
+    @ApiOperation(value = "나의 피드 리스트 조회")
     public BaseResponse<ProfileFeedsResponse> getMyFeeds(@Valid FeedsRequest feedsRequest) {
         return BaseResponse.<ProfileFeedsResponse>builder()
                 .data(feedService.getMyFeeds(feedsRequest))
@@ -105,6 +111,7 @@ public class FeedController {
                     )
             })
     @GetMapping("/{memberId}/list")
+    @ApiOperation(value = "다른 멤버의 피드 리스트 조회")
     public BaseResponse<ProfileFeedsResponse> getFeedsByMemberId(@Valid MemberFeedsRequest memberFeedsRequest) {
         return BaseResponse.<ProfileFeedsResponse>builder()
                 .data(feedService.getFeedsByMemberId(memberFeedsRequest))
@@ -112,6 +119,7 @@ public class FeedController {
     }
 
     @GetMapping("/{feedId}")
+    @ApiOperation(value = "피드 상세 조회")
     public BaseResponse<FeedDetailResponse> getFeedDetailById(@PathVariable @NumberFormat Long feedId) {
         return BaseResponse.<FeedDetailResponse>builder()
                 .data(feedService.getFeedDetailById(feedId))
@@ -138,6 +146,7 @@ public class FeedController {
                             , defaultValue = "10"
                     )
             })
+    @ApiOperation(value = "피드 리스트 조회")
     @GetMapping
     public BaseResponse<FeedPageResponse> getFeedsByFeedPage(@Valid FeedsRequest feedsRequest) {
         return BaseResponse.<FeedPageResponse>builder()
