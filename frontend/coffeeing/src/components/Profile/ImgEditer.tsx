6f7d427c112537ed08eee685ea626b50ff031b18
inputTitleHere <seanbryan97@naver.com>
2023-09-25 14:53:39 +0900

    remove: 미사용 tsx 파일 제거

--- frontend/coffeeing/src/components/Profile/ImgEditer.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export const ImgEditer=()=>{
-  return (
-    <div>
-      halp
-    </div>
-  )
-}
--- frontend/coffeeing/src/components/Profile/ImgModal.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-type ModalControl = {
-  setModalOpen: (isOpen: boolean) => void;
-};
-
-export const ImgModal = ({ setModalOpen }: ModalControl) => {
-  return <div>halp</div>;
-};
