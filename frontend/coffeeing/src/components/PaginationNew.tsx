58ee9a3948492cbfc07cafd3a6867af32437d1e4
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-09-26 13:56:53 +0900

    feat: 새로운 pagination 추가

--- frontend/coffeeing/src/components/PaginationNew.tsx
+++ frontend/coffeeing/src/components/PaginationNew.tsx
@@ -3,29 +3,53 @@ import Next from 'assets/nextarrow.png'
 import Prev from 'assets/prevarrow.png'
 import DisNext from 'assets/disnextarrow.svg';
 import DisPrev from 'assets/disprevarrow.svg'
+import { CapsuleCard } from "./CapsuleCard";
 
 type Product = {
   id:number,
   nameEng:string,
-  brand?:string,
+  nameKr:string,
+  brand?:string ,
   origin?:string
-  imageUrl?:string
+  imageUrl:string
 }
 
-type PaginationProps = {
+export type PaginationProps = {
   currentPage:number,
-  isLast:boolean,
   totalPage:number,
   products:Product[],
   setCurrentPage:Dispatch<React.SetStateAction<number>>
 }
 
-export const PaginationNew = ({currentPage, isLast, totalPage,products,setCurrentPage}:PaginationProps)=>{
+export const PaginationNew = ({currentPage,totalPage,products,setCurrentPage}:PaginationProps)=>{
   const commonClass = 'w-10 h-10 font-semibold text-base';
+  const maxButtons = 10
+  const startPage= Math.max(1, currentPage-Math.floor(maxButtons/2))
+  const endPage = Math.min(totalPage, startPage+maxButtons-1)
   return(
     <div>
+      {/* 카드들 */}
+      <div className="flex flex-wrap w-300 justify-between">
+        {products.map((item)=>{
+          return(
+            <CapsuleCard 
+              capsule_id={item.id} 
+              brand="추후 받아와서 연결해줘야 함" 
+              name={item.nameKr} 
+              imgLink={item.imageUrl}
+              key={item.id} 
+            />
+          )
+        })}
+      </div>
       {/* 페이지네이션 버튼들 */}
       <div className="flex justify-center">
+        {/* 맨 처음 버튼 */}
+        <button
+          onClick={()=>setCurrentPage(1)}
+          disabled={currentPage==1}
+        >처음으로</button>
+        {/* 이전 버튼 */}
         <button
           onClick={() => setCurrentPage(currentPage-1)}
           disabled={currentPage === 1}
@@ -33,19 +57,17 @@ export const PaginationNew = ({currentPage, isLast, totalPage,products,setCurren
         >
           <img src={currentPage === 1 ? DisPrev : Prev} />
         </button>
-        {/* {pageNums.map((item, index) => (
+        {/* 페이지 숫자들 */}
+        {Array.from({ length: endPage-startPage }, (_, index) => (
           <button
             key={index + 1}
-            onClick={() => setCurrentPage(index + 1)}
-            className={
-              index + 1 === currentPage
-                ? `${commonClass} text-light-roasting`
-                : `${commonClass}`
-            }
+            onClick={() => setCurrentPage(startPage+index)}
+            className={startPage+index=== currentPage ? `${commonClass} text-light-roasting` : `${commonClass}`}
           >
-            {index + 1}
+            {startPage+index}
           </button>
-        ))} */}
+        ))}
+        {/* 다음 버튼 */}
         <button
           onClick={() => setCurrentPage(currentPage+1)}
           disabled={currentPage === totalPage}
@@ -53,6 +75,12 @@ export const PaginationNew = ({currentPage, isLast, totalPage,products,setCurren
         >
           <img src={currentPage === totalPage ? DisNext : Next} />
         </button>
+        {/* 맨 뒤로 버튼 */}
+        <button
+          onClick={()=>setCurrentPage(totalPage)}
+          disabled={currentPage===totalPage}
+          className="pointer"
+        >맨 뒤로</button>
       </div>
     </div>
   )
