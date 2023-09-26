b3d7d9e87c858d0146860d364100c5186f9b520d
백승윤 <seanbryan97@naver.com>
2023-09-26 13:19:54 +0900

    fix: nickname checking regex 수정

---
+++ backend/coffeeing/src/test/java/com/ssafy/coffeeing/regex/RegexTest.java
@@ -0,0 +1,33 @@
+package com.ssafy.coffeeing.regex;
+
+import com.ssafy.coffeeing.modules.member.dto.NicknameChangeRequest;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class RegexTest {
+    @Test
+    public void testNicknameRegex() {
+        String input = "nickname";
+        String regex = "^(?!\\s)(?!.*\\s$)(?!.*\\s{2}).*$";
+        String regex2 = "^[\\w\\s가-힣ㄱ-ㅎ]*$";
+
+        Pattern pattern = Pattern.compile(regex2);
+        Matcher matcher = pattern.matcher(input);
+
+        assertTrue(matcher.find());
+    }
+}
