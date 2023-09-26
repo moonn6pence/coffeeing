fa3bb2b4d2c7e481053239c7434119d7270a27a6
hanulkimm <hanulkim2000@naver.com>
2023-09-27 00:25:33 +0900

    feat: 필터 태그 삭제 기능 추가

--- frontend/coffeeing/src/components/Search/SelectedTag.tsx
+++ frontend/coffeeing/src/components/Search/SelectedTag.tsx
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import { FilterItem } from "util/constants";
-
+import IonIcon from "@reacticons/ionicons";
 type SelectedTagProps = {
   src: string;
   selectedItem: FilterItem[];
+  setSelectedItem: Dispatch<SetStateAction<FilterItem[]>>;
 };
 
-export const SelectedFilterTag = ({ src, selectedItem }: SelectedTagProps) => {
+export const SelectedFilterTag = ({ src, selectedItem,setSelectedItem }: SelectedTagProps) => {
+  const removeItem = (itemToRemove: FilterItem) => {
+    const updatedItems = selectedItem.filter((item) => item.name !== itemToRemove.name);
+    setSelectedItem(updatedItems); 
+  };
   return (
     <div className="flex flex-row gap-1">
       {selectedItem
         .slice(1,)
         .map((item) => (
-        <div key={item.name} className="w-fit p-4 h-8 border border-cinamon-roasting rounded-3xl flex gap-2 items-center justify-center">
+        <div key={item.name} className="w-fit p-4 h-8 border rounded-3xl flex gap-2 items-center justify-center bg-half-light">
           <img src={src} alt={item.name} className="w-4 h-5" />
           {item.name}
+          <IonIcon onClick={()=>removeItem(item)} name="close-outline"></IonIcon>
         </div>
       ))}
     </div>
