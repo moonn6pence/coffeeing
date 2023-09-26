d0107f6b99dc3ab96586c3a60ed8e627bb2673e0
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-09-26 10:52:31 +0900

    fix : 검색 요청 api 연결 수정
    빈 값이면 params에  애초에 안 보내기

--- frontend/coffeeing/src/service/filter/request.ts
+++ frontend/coffeeing/src/service/filter/request.ts
@@ -1,22 +1,42 @@
 import { API_URL } from "util/constants"
 import { publicRequest } from "util/axios"
-import { FilterProps } from "./types"
+import { FilterProps, FilterSend } from "./types"
 
 export const requestFilterResult = ({selectedRoast, selectedAcid,selectedBody,selectedFlavorNote, keyword,productType, page}:FilterProps) => {
   const sendRoast = selectedRoast.slice(1).map(item => item.label).join(', ');
   const sendAcidity = selectedAcid.slice(1).map(item => item.label).join(', ');
   const sendBody = selectedBody.slice(1).map(item => item.label).join(', ');
   const sendFlavorNote = selectedFlavorNote.slice(1).map(item => item.label).join(',');
+  // const params = {roast:sendRoast, acidity:sendAcidity, body:sendBody, flavorNote:sendFlavorNote,keyword:keyword,productType:productType,page:page,size:8}
+  const params : FilterSend = {
+    roast:sendRoast,
+    acidity:sendAcidity,
+    body: sendBody,
+    flavorNote:sendFlavorNote,
+    keyword:keyword,
+    productType:productType,
+    page:page,
+    size:8,
+  }
+  if (!sendRoast) {
+    delete params.roast
+  }
+  if (!sendAcidity) {
+    delete params.acidity
+  }
+  if (!sendBody) {
+    delete params.body
+  }
+  if (!sendFlavorNote) {
+    delete params.flavorNote
+  }
 
-  const params = {roast:sendRoast, acidity:sendAcidity, body:sendBody, flavorNote:sendFlavorNote,keyword:keyword,productType:productType,page:page,size:8}
-  console.log(params)
   publicRequest
   .get(`${API_URL}/search/products`,{params})
   .then((res)=>{
     return res.data
   })
   .catch((err)=>{
-    console.log("[filter request fail]")
-    return err
+    console.log("[filter request fail]",err.response.data.message)
   })
 }
