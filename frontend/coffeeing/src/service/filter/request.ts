ecdac9a83d1d2b7f250420418d8eb06708bb1149
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-09-26 11:29:09 +0900

    fix: 비동기 요청 수정

--- frontend/coffeeing/src/service/filter/request.ts
+++ frontend/coffeeing/src/service/filter/request.ts
@@ -2,12 +2,12 @@ import { API_URL } from "util/constants"
 import { publicRequest } from "util/axios"
 import { FilterProps, FilterSend } from "./types"
 
-export const requestFilterResult = ({selectedRoast, selectedAcid,selectedBody,selectedFlavorNote, keyword,productType, page}:FilterProps) => {
+export const requestFilterResult = async ({selectedRoast, selectedAcid,selectedBody,selectedFlavorNote, keyword,productType, page}:FilterProps) => {
   const sendRoast = selectedRoast.slice(1).map(item => item.label).join(', ');
   const sendAcidity = selectedAcid.slice(1).map(item => item.label).join(', ');
   const sendBody = selectedBody.slice(1).map(item => item.label).join(', ');
   const sendFlavorNote = selectedFlavorNote.slice(1).map(item => item.label).join(',');
-  // const params = {roast:sendRoast, acidity:sendAcidity, body:sendBody, flavorNote:sendFlavorNote,keyword:keyword,productType:productType,page:page,size:8}
+  
   const params : FilterSend = {
     roast:sendRoast,
     acidity:sendAcidity,
@@ -30,13 +30,14 @@ export const requestFilterResult = ({selectedRoast, selectedAcid,selectedBody,se
   if (!sendFlavorNote) {
     delete params.flavorNote
   }
-
-  publicRequest
-  .get(`${API_URL}/search/products`,{params})
-  .then((res)=>{
-    return res.data
-  })
-  .catch((err)=>{
-    console.log("[filter request fail]",err.response.data.message)
-  })
+  if (!keyword) {
+    delete params.keyword
+  }
+  try {
+    const response = await publicRequest.get(`${API_URL}/search/products`, { params });
+    return response.data.data; 
+  } catch (err) {
+    console.log("[filter request fail]", err);
+    throw err;
+  }
 }
