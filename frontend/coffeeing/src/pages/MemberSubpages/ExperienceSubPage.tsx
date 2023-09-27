a61ac11561ac37c8966e89049643cbfd91c85f4f
inputTitleHere <seanbryan97@naver.com>
2023-09-27 11:23:49 +0900

    feat: 경험치 정보 표시 기능 추가

--- frontend/coffeeing/src/pages/MemberSubpages/ExperienceSubPage.tsx
+++ frontend/coffeeing/src/pages/MemberSubpages/ExperienceSubPage.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { useOutletContext } from 'react-router-dom';
 import { privateRequest } from 'util/axios';
 import { API_URL } from 'util/constants';
+import IonIcon from '@reacticons/ionicons';
 
 type ExperienceInfo = {
   experience: number;
@@ -15,6 +16,7 @@ export const ExperienceSubPage = () => {
   const [experienceInfo, setExperienceInfo] = useState<
     ExperienceInfo | undefined
   >(undefined);
+  const [infoTabOpen, setInfoTabOpen] = useState(false);
 
   useEffect(() => {
     // get user experience
@@ -26,22 +28,37 @@ export const ExperienceSubPage = () => {
       });
   }, []);
 
+  const handleInfoToggle = (toggle: boolean) => {
+    setInfoTabOpen(toggle);
+  };
+
   return (
     <div className="sub-wrapper">
       {experienceInfo ? (
         <div className="wrapper">
-          <div className="header">
-            <div className="font-bold text-3xl">
+          <div className="header flex items-center justify-between relative">
+            <span className="font-bold text-3xl">
               LV : {experienceInfo.memberLevel}
-            </div>
+            </span>
+            <IonIcon
+              name="alert-circle-outline"
+              className="font-bold text-2xl cursor-pointer"
+              onMouseEnter={() => handleInfoToggle(true)}
+              onMouseLeave={() => handleInfoToggle(false)}
+            />
+            {infoTabOpen ? <div className='absolute right-0 top-10 z-20 border-2 p-5 bg-light rounded'>
+              <ul>
+                <li>피드 글 작성 + 75</li>
+                <li>리뷰 작성 + 50</li>
+                <li>추천 받기 + 15</li>
+              </ul>
+            </div> : ''}
           </div>
-          <div className="image-wrapper">
-            PLACEHOLDER FOR IMAGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGE
-          </div>
+          <div className="image-wrapper">PLACEHOLDER FOR image</div>
           <div className="progress-wrapper">
-            <div className="progress-white w-full bg-light h-10 rounded-xl">
+            <div className="progress-white w-full bg-light h-10 rounded-xl relative flex items-center justify-end">
               <div
-                className={`progress-inner bg-light-roasting h-10 rounded-xl`}
+                className={`progress-inner bg-light-roasting h-10 rounded-xl absolute top-0 left-0`}
                 style={{
                   width:
                     (experienceInfo.experience /
@@ -50,6 +67,10 @@ export const ExperienceSubPage = () => {
                     '%',
                 }}
               ></div>
+              <span className="font-bold z-10 mr-5">
+                {experienceInfo.experience} /{' '}
+                {experienceInfo.experienceForLevelUp}
+              </span>
             </div>
           </div>
         </div>
