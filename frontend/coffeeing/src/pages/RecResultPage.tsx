e8164a6fe7351a30f286e882b42daf0611497b40
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-09-27 15:27:20 +0900

    feature: 추천 결과 페이지 만드는 중

--- frontend/coffeeing/src/pages/RecResultPage.tsx
+++ frontend/coffeeing/src/pages/RecResultPage.tsx
@@ -2,11 +2,43 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { getSurveyResult, savePreference } from "service/survey/recommend";
 import { RootState } from "store/store";
-import { privateRequest } from "util/axios";
-import { API_URL } from "util/constants";
+import { BeanCard } from "components/BeanCard";
+
+type ProductsType = {
+  id:number,
+  imageUrl:string,
+  subtitle:string,
+  title:string,
+}
+
+type ResultType = {
+  roast:number,
+  acidity:number,
+  body:number,
+  imageUrl:string,
+  nickname:string,
+  recommendation:{
+    isCapsule:boolean,
+    products: ProductsType[]
+  },
+}
 
 export const RecResultPage  = ()=>{
-  const [rec, setRec] = useState([]);
+  // 타입 추후 수정
+  const [rec, setRec] = useState<ResultType>();
+  const [userInfo, setUserInfo] = useState({
+    roast:0,
+    acidity:0,
+    body:0,
+    imageUrl:'',
+    nickname:'',
+  })
+  const [products, setProducts] = useState([{
+    id:0,
+    imageUrl:'',
+    subtitle:'',
+    title:''
+  }])
   const survey =useSelector((state:RootState)=>state.survey);
   const sendPreference = async ()=>{
     const result = await savePreference(survey);
@@ -14,13 +46,30 @@ export const RecResultPage  = ()=>{
   }
   const getPreference = async ()=>{
     const result = await getSurveyResult(survey);
-    // setRec(result)
+    result 
+    ? 
+      setProducts(result.products)
+    : console.log(result)
   }
   useEffect( ()=>{
     sendPreference();
   },[])
 
   return(
-    <div>아</div>
+    <div>
+      <div className="flex w-300 justify-between">
+        {/* {products.map((item, index) => (
+          <BeanCard
+            subtitle={item.subtitle}
+            name={item.title}
+            id={item.id}
+            imgLink={item.imageUrl}
+            isCapsule={beans === 'capsule' ? true : false}
+            key={index}
+          />
+        ))} */}
+      </div>
+
+    </div>
   )
 }
