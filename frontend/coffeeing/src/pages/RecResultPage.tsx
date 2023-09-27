9889b87f7414c793ea1231c80b3ece94229fb540
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-09-27 13:28:17 +0900

    fix : 설문 결과 받는 axios 수정

--- frontend/coffeeing/src/pages/RecResultPage.tsx
+++ frontend/coffeeing/src/pages/RecResultPage.tsx
@@ -1,78 +1,24 @@
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { getSurveyResult, savePreference } from "service/survey/recommend";
+import { RootState } from "store/store";
 import { privateRequest } from "util/axios";
 import { API_URL } from "util/constants";
 
 export const RecResultPage  = ()=>{
-  const [result, setResult] = useState();
-
+  const [rec, setRec] = useState([]);
+  const survey =useSelector((state:RootState)=>state.survey);
   const sendPreference = async ()=>{
-    try {
-      return await savePreference();
-    } catch (error) {
-      console.error('Error sending preference:', error)
-    }
+    const result = await savePreference(survey);
+    console.log(result)
   }
-  
+  const getPreference = async ()=>{
+    const result = await getSurveyResult(survey);
+    // setRec(result)
+  }
   useEffect( ()=>{
     sendPreference();
   },[])
-  // useEffect(()=>{
-  //   console.log(survey)
-  //   privateRequest
-  //   .post(`${API_URL}/survey/save`, 
-  //     {},
-  //     {
-  //       params: {
-  //       roast:survey.roasting,
-  //       acidity:survey.acidity,
-  //       body:survey.body,
-  //       flavorNote:survey.flavorNote,
-  //       isCapsule:survey.isCapsule,
-  //       machineType:survey.machine
-  //       } 
-  //     }
-  //   )
-  //   .then((res)=>{
-  //     console.log(res.data)
-  //     setResult(res.data.data)
-  //   })
-  //   .catch((err)=>{
-  //     console.log(err)
-  //   })
-  // },[])
-
-  // useEffect(()=>{
-  //   async () => {
-  //     const res = await savePreference({
-  //       roast:survey.roasting,
-  //       acidity:survey.acidity,
-  //       body:survey.body,
-  //       flavorNote:survey.flavorNote,
-  //       isCapsule:true,
-  //       machineType:survey.machine
-  //     })
-  //     if (res) {
-  //       console.log('[save preference succeeded]')
-  //     } else {
-  //       console.log('[save preference failed')
-  //     }
-  //   }
-  // },[])
-
-  // useEffect(()=>{
-  //   async () => {
-  //     const surveyResult = await getSurveyResult({
-  //       roast:survey.roasting,
-  //       acidity:survey.acidity,
-  //       body:survey.body,
-  //       flavorNote:survey.flavorNote,
-  //       isCapsule:true,
-  //       machineType:survey.machine
-  //     })
-  //     // setResult(surveyResult)
-  //   }
-  // },[])
 
   return(
     <div>아</div>
