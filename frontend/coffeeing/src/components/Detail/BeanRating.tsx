80c8c9fbe542c791f5a9a016e0f5c3e2e3dac691
hyuna <kook980316@gmail.com>
2023-09-27 10:04:33 +0900

    refactor: 로스트, 바디, 산미 부분 분리

--- frontend/coffeeing/src/components/Detail/BeanRating.tsx
+++ frontend/coffeeing/src/components/Detail/BeanRating.tsx
@@ -3,30 +3,55 @@ import zeroBean from 'assets/detail/zero_bean.png';
 import halfBean from 'assets/detail/half_bean.png';
 import oneBean from 'assets/detail/one_bean.png';
 
-export const BeanRating = ({ rate }: { rate: number }) => {
+type BeanRatingProps = {
+  acidity: number;
+  roast: number;
+  body: number;
+};
+
+export const BeanRating = (props: BeanRatingProps) => {
+  const { acidity, roast, body } = props;
+
+  const beanCharac = [
+    { name: '로스팅', value: roast },
+    { name: '바디감', value: body },
+    { name: '산 미', value: acidity },
+  ];
+
   const commonSize = 'w-12 h-12';
   // 콩 이미지를 저장할 배열
-  const beansRating = [];
-
-  // 이미지 저장 기능
-  for (let i = 0; i < 5; i++) {
-    // 현재 인덱스 i와 평점을 비교
-    if (i < Math.floor(rate)) {
-      // 현재 인덱스가 평점 미만이면 oneBean
-      beansRating.push(
-        <img key={i} src={oneBean} alt="한 콩" className={commonSize} />,
-      );
-    } else if (i === Math.floor(rate) && rate % 1 !== 0) {
-      // 현재 인덱스가 평점과 같고, 평점이 소수일 때 halfBean
-      beansRating.push(
-        <img key={i} src={halfBean} alt="반 콩" className={commonSize} />,
-      );
-    } else {
-      beansRating.push(
-        <img key={i} src={zeroBean} alt="빈 콩" className={commonSize} />,
-      );
+  const beansRating = (rate: number) => {
+    const beans = [];
+    // 이미지 저장 기능
+    for (let i = 0; i < 5; i++) {
+      // 현재 인덱스 i와 평점을 비교
+      if (i < Math.floor(rate)) {
+        // 현재 인덱스가 평점 미만이면 oneBean
+        beans.push(
+          <img key={i} src={oneBean} alt="한 콩" className={commonSize} />,
+        );
+      } else if (i === Math.floor(rate) && rate % 1 !== 0) {
+        // 현재 인덱스가 평점과 같고, 평점이 소수일 때 halfBean
+        beans.push(
+          <img key={i} src={halfBean} alt="반 콩" className={commonSize} />,
+        );
+      } else {
+        beans.push(
+          <img key={i} src={zeroBean} alt="빈 콩" className={commonSize} />,
+        );
+      }
     }
-  }
+    return beans;
+  };
 
-  return <div className="flex space-x-3">{beansRating}</div>;
+  return (
+    <div className="flex flex-col space-y-3">
+      {beanCharac.map((charac) => (
+        <div className="flex w-full justify-between" key={charac.name}>
+          <span className="text-xl font-bold my-auto">{charac.name}</span>
+          <div className="flex space-x-3">{beansRating(charac.value)}</div>
+        </div>
+      ))}
+    </div>
+  );
 };
