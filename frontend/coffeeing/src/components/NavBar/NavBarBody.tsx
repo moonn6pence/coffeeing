912fd8025e47a5b1f81a250770ad8851c8fe86b0
hyuna333 <kook980316@gmail.com>
2023-09-30 01:52:39 +0900

    design: navbar width 변경

--- frontend/coffeeing/src/components/NavBar/NavBarBody.tsx
+++ frontend/coffeeing/src/components/NavBar/NavBarBody.tsx
@@ -12,8 +12,8 @@ export const NavBarBody = () => {
   const myInfo = useSelector((state:RootState) => state.member);
   const navigate = useNavigate();
   return (
-    <div className="w-screen h-16 border-b border-light-roasting py-2 px-30">
-      <div className="flex justify-between w-300">
+    <div className="w-screen h-16 border-b border-light-roasting py-2 flex justify-center">
+      <div className="flex justify-between w-4/5">
         <button
           onClick={() => {
             navigate('/');
