04264e577b3b3df7d6a44274656fb55ad607a03b
hanulkimm <hanulkim2000@naver.com>
2023-09-29 23:19:30 +0900

    fix: 동적 css 수정

--- frontend/coffeeing/src/components/Survey/RadioGroup.tsx
+++ frontend/coffeeing/src/components/Survey/RadioGroup.tsx
@@ -1,29 +1,38 @@
 import React from "react";
 import { RadioGroup } from "@headlessui/react";
-import { CoffeeItem } from "util/constants";
-
+import { CoffeeItem} from "util/constants";
+import { RoastingTooltip } from "components/Tooltip/survey";
 type RadioGroupProps = {
-  selectedItem:string,
+  selectedItem:string|number,
   setSelectedItem:any,
   itemList:CoffeeItem[],
+  pageNum:number,
 }
 
-export const RadioGroupSingle = ({selectedItem, setSelectedItem, itemList}:RadioGroupProps)=>{
+export const RadioGroupSingle = ({selectedItem, setSelectedItem, itemList,pageNum}:RadioGroupProps)=>{
   return(
     <RadioGroup className="flex gap-10" value={selectedItem} onChange={setSelectedItem}>
       {itemList.map((item)=>(
         <RadioGroup.Option key={item.label} value={item.label}>
           {({checked})=>(
             <div
-            className={`w-96 h-80 flex flex-col items-center ${
-              checked ? 'bg-select-img' : ''
-            } rounded-xl`}
+            className={
+              `${pageNum===0?'w-96 h-80':'w-64 h-60'}
+              flex flex-col items-center justify-end rounded-xl
+              ${checked ? 'bg-select-img' : ''} `}
             >
             <img
-              className={`w-80 h-72  origin-center transform hover:scale-105 hover:translate-y-[-10px] `}
+              className={
+                `${pageNum===0?'w-80 h-72':pageNum===1?'w-48 h-48':'w-52 h-52'} 
+                origin-center transform hover:scale-105 hover:translate-y-[-10px] `}
               src={item.src}
             />
-            <p className="text-xl">{item.name}</p>
+            <div className="flex flex-row items-baseline gap-1">
+              <p className={`${pageNum===0?'text-xl mb-2':'mb-2'}`}>{item.name}</p>
+              {pageNum===1&&item.toolTipDesc&&
+                <RoastingTooltip label={item.toolTipDesc}/>
+              }
+            </div>
           </div>
           )}
         </RadioGroup.Option>
