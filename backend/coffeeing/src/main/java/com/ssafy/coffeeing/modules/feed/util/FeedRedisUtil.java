149fbc641dbb94c18ef45d007c68116ddb9dbd7c
김태용 <amico741@naver.com>
2023-10-02 16:48:19 +0900

    feat: feedLike 토글에 대한 Redis 로직 추가

--- backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/feed/util/FeedRedisUtil.java
+++ backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/feed/util/FeedRedisUtil.java
@@ -1,10 +1,14 @@
 package com.ssafy.coffeeing.modules.feed.util;
 
+import com.ssafy.coffeeing.modules.feed.domain.Feed;
+import com.ssafy.coffeeing.modules.member.domain.Member;
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class FeedRedisUtil {
@@ -15,5 +19,51 @@ public class FeedRedisUtil {
         this.redisTemplate = redisTemplate;
         this.redisTemplate.setHashKeySerializer(new Jackson2JsonRedisSerializer<>(Long.class));
         this.redisTemplate.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(HashMap.class));
+    }
+
+    public boolean isLikedFeedInRedis(Feed feed, Member member) {
+        HashOperations<String, Long, HashMap> hashOperations = redisTemplate.opsForHash();
+
+        if (hashOperations.hasKey("feedLike", feed.getId())) {
+            Map<Long, Boolean> feedLikeCache = hashOperations.get("feedLike", feed.getId());
+
+            if (feedLikeCache.containsKey(member.getId())) {
+                return feedLikeCache.get(member.getId());
+            }
+            return false;
+        }
+        return false;
+    }
+
+    public boolean isNotLikedFeedInRedis(Feed feed, Member member) {
+        return !isLikedFeedInRedis(feed, member);
+    }
+
+    public void disLikeFeedInRedis(Feed feed, Member member) {
+        HashOperations<String, Long, HashMap> hashOperations = redisTemplate.opsForHash();
+        HashMap<Long, Boolean> feedLikeCache;
+
+        if (hashOperations.hasKey("feedLike", feed.getId())) {
+            feedLikeCache = hashOperations.get("feedLike", feed.getId());
+            feedLikeCache.put(member.getId(), false);
+        } else {
+            feedLikeCache = new HashMap<>();
+            feedLikeCache.put(member.getId(), false);
+        }
+        hashOperations.put("feedLike", feed.getId(), feedLikeCache);
+    }
+
+    public void likeFeedInRedis(Feed feed, Member member) {
+        HashOperations<String, Long, HashMap> hashOperations = redisTemplate.opsForHash();
+        HashMap<Long, Boolean> feedLikeCache;
+
+        if (hashOperations.hasKey("feedLike", feed.getId())) {
+            feedLikeCache = hashOperations.get("feedLike", feed.getId());
+            feedLikeCache.put(member.getId(), true);
+        } else {
+            feedLikeCache = new HashMap<>();
+            feedLikeCache.put(member.getId(), true);
+        }
+        hashOperations.put("feedLike", feed.getId(), feedLikeCache);
     }
 }
