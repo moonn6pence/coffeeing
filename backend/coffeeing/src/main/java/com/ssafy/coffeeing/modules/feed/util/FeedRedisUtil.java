0a71564c205225e72dcb47abc9b37b75b051ebfc
김태용 <amico741@naver.com>
2023-10-02 18:46:35 +0900

    refactor: expireTime 설정으로 인한 read Through 전략으로 리팩토링

--- backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/feed/util/FeedRedisUtil.java
+++ backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/feed/util/FeedRedisUtil.java
@@ -1,6 +1,8 @@
 package com.ssafy.coffeeing.modules.feed.util;
 
 import com.ssafy.coffeeing.modules.feed.domain.Feed;
+import com.ssafy.coffeeing.modules.feed.domain.FeedLike;
+import com.ssafy.coffeeing.modules.feed.repository.FeedLikeRepository;
 import com.ssafy.coffeeing.modules.member.domain.Member;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -8,31 +10,57 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
-import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 @Component
 public class FeedRedisUtil {
 
     private final RedisTemplate redisTemplate;
+    private final FeedLikeRepository feedLikeRepository;
 
-    public FeedRedisUtil(RedisTemplate redisTemplate) {
+    private final String KEY = "feedLike";
+
+    public FeedRedisUtil(RedisTemplate redisTemplate, FeedLikeRepository feedLikeRepository) {
         this.redisTemplate = redisTemplate;
         this.redisTemplate.setHashKeySerializer(new Jackson2JsonRedisSerializer<>(Long.class));
+        this.feedLikeRepository = feedLikeRepository;
     }
 
     public boolean isLikedFeedInRedis(Feed feed, Member member) {
         HashOperations<String, Long, HashMap> hashOperations = redisTemplate.opsForHash();
+        HashMap<Long, Boolean> feedLikeCache;
 
-        if (hashOperations.hasKey("feedLike", feed.getId())) {
-            Map<Long, Boolean> feedLikeCache = hashOperations.get("feedLike", feed.getId());
-
-            if (feedLikeCache.containsKey(member.getId())) {
-                return feedLikeCache.get(member.getId());
+        if (hashOperations.hasKey(KEY, feed.getId())) {
+            feedLikeCache = hashOperations.get(KEY, feed.getId());
+            if (!feedLikeCache.containsKey(member.getId())) {
+                setFeedLikeStatus(feed, member, hashOperations, feedLikeCache);
             }
-            return false;
+        } else { // Read Through 전략
+            feedLikeCache = new HashMap<>();
+            setFeedLikeStatus(feed, member, hashOperations, feedLikeCache);
+
+            if (isNotSetExpireTime()) {
+                redisTemplate.expire(KEY, 8, TimeUnit.HOURS);
+            }
         }
-        return false;
+        return feedLikeCache.get(member.getId());
+    }
+
+    private void setFeedLikeStatus(
+            Feed feed,
+            Member member,
+            HashOperations<String, Long, HashMap> hashOperations,
+            HashMap<Long, Boolean> feedLikeCache) {
+        Optional<FeedLike> feedLike = feedLikeRepository.findFeedLikeByFeedAndMember(feed, member);
+
+        if (feedLike.isPresent()) {
+            feedLikeCache.put(member.getId(), true);
+        } else {
+            feedLikeCache.put(member.getId(), false);
+        }
+        hashOperations.put(KEY, feed.getId(), feedLikeCache);
     }
 
     public boolean isNotLikedFeedInRedis(Feed feed, Member member) {
@@ -41,41 +69,30 @@ public class FeedRedisUtil {
 
     public void disLikeFeedInRedis(Feed feed, Member member) {
         HashOperations<String, Long, HashMap> hashOperations = redisTemplate.opsForHash();
-        HashMap<Long, Boolean> feedLikeCache;
 
-        if (hashOperations.hasKey("feedLike", feed.getId())) {
-            feedLikeCache = hashOperations.get("feedLike", feed.getId());
-            feedLikeCache.put(member.getId(), false);
-        } else {
-            feedLikeCache = new HashMap<>();
-            feedLikeCache.put(member.getId(), false);
-        }
-        hashOperations.put("feedLike", feed.getId(), feedLikeCache);
+        HashMap<Long, Boolean> feedLikeCache = hashOperations.get(KEY, feed.getId());
 
-        if (isNotSetExpireTime()) {
-            redisTemplate.expire("feedLike", 8, TimeUnit.HOURS);
+        if (Objects.nonNull(feedLikeCache)) {
+            feedLikeCache.put(member.getId(), false);
+            hashOperations.put(KEY, feed.getId(), feedLikeCache);
         }
     }
 
     public void likeFeedInRedis(Feed feed, Member member) {
         HashOperations<String, Long, HashMap> hashOperations = redisTemplate.opsForHash();
-        HashMap<Long, Boolean> feedLikeCache;
 
-        if (hashOperations.hasKey("feedLike", feed.getId())) {
-            feedLikeCache = hashOperations.get("feedLike", feed.getId());
-            feedLikeCache.put(member.getId(), true);
-        } else {
-            feedLikeCache = new HashMap<>();
-            feedLikeCache.put(member.getId(), true);
-        }
-        hashOperations.put("feedLike", feed.getId(), feedLikeCache);
+        HashMap<Long, Boolean> feedLikeCache = hashOperations.get(KEY, feed.getId());
 
-        if (isNotSetExpireTime()) {
-            redisTemplate.expire("feedLike", 8, TimeUnit.HOURS);
+        if(Objects.nonNull(feedLikeCache)) {
+            feedLikeCache.put(member.getId(), true);
+            hashOperations.put(KEY, feed.getId(), feedLikeCache);
         }
     }
 
     private boolean isNotSetExpireTime() {
-        return redisTemplate.getExpire("feedLike").equals(-1L);
+        if (Objects.nonNull(redisTemplate.getExpire(KEY))) {
+            return redisTemplate.getExpire(KEY).equals(-1L);
+        }
+        return false;
     }
 }
