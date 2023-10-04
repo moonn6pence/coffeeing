0146067f78123f313564e5633013d97385423362
백승윤 <seanbryan97@naver.com>
2023-10-04 16:58:07 +0900

    feat: member 경험치 관련 코드 수정 및 레벨 리밋 추가

--- backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/member/util/MemberUtil.java
+++ backend/coffeeing/src/main/java/com/ssafy/coffeeing/modules/member/util/MemberUtil.java
@@ -5,7 +5,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MemberUtil {
+    // An+1 = An + (125  + 75*n)
+    private static final int[] EXPERIENCE_LIMIT = {0,125,325,600};
+    private static final int LEVEL_LIMIT = 3;
     public int calculateLevelUpExperience(int level){
-        return level * Experience.ADDITIONAL_EXPERIENCE_PER_LEVEL.getValue() + Experience.INITIAL_EXPERIENCE.getValue();
+//        return level * Experience.ADDITIONAL_EXPERIENCE_PER_LEVEL.getValue() + Experience.INITIAL_EXPERIENCE.getValue();
+        if(level==LEVEL_LIMIT){
+            return EXPERIENCE_LIMIT[LEVEL_LIMIT];
+        }
+        return EXPERIENCE_LIMIT[level+1];
     }
 }
