0867ab422eef78836c34c155e479f92de680f80a
Hanul Kim <hanulkim2000@korea.ac.kr>
2023-10-04 15:06:08 +0900

    refactor: 추천 결과 저장 버튼 & 반응형 추가

--- frontend/coffeeing/src/pages/RecResultPage.tsx
+++ frontend/coffeeing/src/pages/RecResultPage.tsx
@@ -8,6 +8,7 @@ import profile from '../assets/profile.svg'
 import { useNavigate } from "react-router-dom";
 import again from '../assets/again.png'
 import loadingGif from '../assets/survey/loading.gif'
+import { Toast } from "components/Toast";
 
 export const RecResultPage  = ()=>{
   const navigate = useNavigate();
@@ -31,10 +32,12 @@ export const RecResultPage  = ()=>{
   }])
   const sendPreference = async ()=>{
     const result = await savePreference(survey);
-    console.log(result)
+    if (result) {
+      Toast.fire('저장되었습니다','','success')
+    }
   }
   const getPreference = async ()=>{
-    const result = await getSurveyResult(survey);
+    const result = await getSurveyResult(survey, isLogin);
     if (result) {
       setProducts(result.recommendation.products)
       setUserInfo((userInfo)=>({
@@ -53,9 +56,6 @@ export const RecResultPage  = ()=>{
   }
 
   useEffect( ()=>{
-    if (isLogin) {
-      sendPreference();
-    }
     getPreference();
   },[])
 
@@ -70,6 +70,7 @@ export const RecResultPage  = ()=>{
       ) 
       : (
         <div>
+          {/* 상단부 */}
           <div className="mt-10 flex flex-col">
             <p className="text-2xl font-bold">
               {userInfo.nickname} <span>{isLogin ? '님' : ''}</span> 맞춤 {userInfo.isCapsule ? '캡슐' : '원두'} 추천
@@ -87,7 +88,9 @@ export const RecResultPage  = ()=>{
               ))}
             </div>
           </div>
+
           <hr className="mx-15" />
+          {/* 추천 다시 받기 */}
           <div className="mt-5 flex flex-row mx-15 justify-end items-center gap-1">
             <img src={again} className="w-4 h-4" alt="Again" />
             <p
@@ -97,18 +100,28 @@ export const RecResultPage  = ()=>{
               추천 다시 받기
             </p>
           </div>
-          <div className="mt-2 bg-light mx-15 h-80 flex items-center justify-around">
+          {/* 하단부 */}
+          <div className="mt-2 bg-light mx-15 h-80 flex items-center justify-center">
             {isLogin&&(
-              <div className="flex flex-col gap-3 items-center w-70.5">
-                <img src={profile} alt="Profile" className="w-44 h-44 rounded-full border-2" />
-                <p>{userInfo.nickname}</p>
+              <div className=" flex flex-col gap-3 items-center w-0 md:w-70.5">
+                <img src={profile} alt="Profile" className="hidden md:block w-44 h-44 rounded-full border-2" />
+                <p className="hidden md:block">{userInfo.nickname}</p>
               </div>
             )}
-            <div className="space-y-3 w-532px">
+            <div className="space-y-3 w-1/2">
               <p className="text-xl font-black mb-10">내 취향 분석</p>
               <BeanRating roast={userInfo.roast*5} acidity={userInfo.acidity*5} body={userInfo.body*5} />
             </div>
           </div>
+          {/* 취향 분석 저장하기 버튼 */}
+          {isLogin&&(
+            <div className="w-full flex justify-center my-2">
+              <p
+                className="bg-half-light text-white p-3 rounded-full cursor-pointer"
+                onClick={()=>sendPreference()}
+                >내 취향 정보 저장하기</p>
+            </div>
+          )}
         </div>
       )}
     </div>
